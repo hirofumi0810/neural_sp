Fix attention mask

models/chainer/attention/attention_layer.py
@@ -157,24 +157,21 @@ class AttentionMechanism(chainer.Chain):
             raise NotImplementedError
 
         # Mask attention distribution
-        # energy_mask = Variable(
-        #     np.ones((batch_size, max_time), dtype=np.float32))
-        # if self.use_cuda:
-        #     energy_mask.to_gpu()
-        # for x_len in x_lens:
-        #     if x_len < max_time:
-        #         energy_mask[:, x_len.data:] = 0
-        # energy *= energy_mask
-
-        # Sharpening
-        energy = energy * self.sharpening_factor
-        # NOTE: energy: `[B, T_in]`
+        energy_mask = Variable(
+            np.ones((batch_size, max_time), dtype=np.float32))
+        if self.use_cuda:
+            energy_mask.to_gpu()
+        for b in range(batch_size):
+            if x_lens[b] < max_time:
+                energy_mask[b, x_lens[b]:] = 0
+        energy *= energy_mask
 
         # Compute attention weights
         if self.sigmoid_smoothing:
-            att_weights_step = F.sigmoid(energy)
+            att_weights_step = F.sigmoid(energy * self.sharpening_factor)
         else:
-            att_weights_step = F.softmax(energy, axis=1)
+            att_weights_step = F.softmax(
+                energy * self.sharpening_factor, axis=1)
 
         # Compute context vector (weighted sum of encoder outputs)
         batch_size, max_time = att_weights_step.shape

models/pytorch/attention/attention_layer.py
@@ -10,6 +10,7 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Variable
 
 from models.pytorch.linear import LinearND
 
@@ -161,26 +162,20 @@ class AttentionMechanism(nn.Module):
             raise NotImplementedError
 
         # Mask attention distribution
-        # energy_mask = Variable(torch.ones(batch_size, max_time))
-        # if enc_out.is_cuda:
-        #     energy_mask = energy_mask.cuda()
-        # for x_len in x_lens:
-        #     if x_len.data[0] < max_time:
-        #         energy_mask[:, x_len.data[0]:] = 0
-        # energy *= energy_mask
-
-        # Sharpening
-        energy *= self.sharpening_factor
-        # NOTE: energy: `[B, T_in]`
-
-        # log_t = math.log(energy.size()[1])
-        # energy = log_t * energy
+        energy_mask = Variable(torch.ones(batch_size, max_time))
+        if enc_out.is_cuda:
+            energy_mask = energy_mask.cuda()
+        for b in range(batch_size):
+            if x_lens[b].data[0] < max_time:
+                energy_mask[b, x_lens[b].data[0]:] = 0
+        energy *= energy_mask
 
         # Compute attention weights
         if self.sigmoid_smoothing:
-            att_weights_step = F.sigmoid(energy)
+            att_weights_step = F.sigmoid(energy * self.sharpening_factor)
         else:
-            att_weights_step = F.softmax(energy, dim=-1)
+            att_weights_step = F.softmax(
+                energy * self.sharpening_factor, dim=-1)
 
         # Compute context vector (weighted sum of encoder outputs)
         context_vec = torch.sum(