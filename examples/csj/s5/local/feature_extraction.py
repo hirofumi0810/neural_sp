4edee049320239f3727fa95b9a6facd356bff687
hirofumi0810 <hiro.mhbc@gmail.com>
2018-03-01 01:06:39 +0900

    Merge CSJ

---
+++ examples/csj/s5/local/feature_extraction.py
@@ -0,0 +1,362 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Make input features (CSJ corpus)."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from os.path import join, basename
+import sys
+import numpy as np
+import pickle
+from tqdm import tqdm
+import argparse
+import codecs
+from collections import OrderedDict
+
+sys.path.append('../../../')
+from utils.directory import mkdir_join
+from utils.feature_extraction.segmentation import segment
+# from utils.feature_extraction.htk import read, write
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_save_path', type=str, help='path to save data')
+
+parser.add_argument('--tool', type=str,
+                    choices=['htk', 'python_speech_features', 'librosa', 'wav'])
+parser.add_argument('--normalize', type=str,
+                    choices=['global', 'speaker', 'utterance', 'no'])
+
+parser.add_argument('--channels', type=int,
+                    help='the number of frequency channels')
+parser.add_argument('--window', type=float, default=0.025,
+                    help='window width to extract features')
+parser.add_argument('--slide', type=float, default=0.01,
+                    help='extract features per slide')
+parser.add_argument('--energy', type=int, help='if 1, add the energy feature')
+parser.add_argument('--delta', type=int, help='if 1, add the energy feature')
+parser.add_argument('--deltadelta', type=int,
+                    help='if 1, double delta features are also extracted')
+
+args = parser.parse_args()
+
+
+CONFIG = {
+    'feature_type': 'fbank',
+    'channels': args.channels,
+    'sampling_rate': 16000,
+    'window': args.window,
+    'slide': args.slide,
+    'energy': bool(args.energy),
+    'delta': bool(args.delta),
+    'deltadelta': bool(args.deltadelta)
+}
+
+
+def main():
+    print('=> Processing input data...')
+    for data_type in ['train', 'dev', 'eval1', 'eval2', 'eval3']:
+
+        print('===> %s' % data_type)
+        feature_save_path = mkdir_join(
+            args.data_save_path, 'feature', args.tool, data_type)
+
+        utt_indices = []
+        with codecs.open(join(args.data_save_path, data_type, 'text'), 'r', 'utf-8') as f:
+            for line in f:
+                line = line.strip()
+                utt_indices.append(line.split('  ')[0])
+
+        segment_dict = {}
+        utt_num = 0
+        with open(join(args.data_save_path, data_type, 'segments'), 'r') as f:
+            for line in f:
+                line = line.strip()
+                utt_idx, speaker, start_time, end_time = line.split(' ')
+
+                if speaker not in segment_dict.keys():
+                    segment_dict[speaker] = OrderedDict()
+                segment_dict[speaker][utt_idx] = [int(float(start_time) * 100 + 0.5),
+                                                  int(float(end_time) * 100 + 0.5)]
+                utt_num += 1
+        assert len(utt_indices) == utt_num
+
+        spk2audio = {}
+        if args.tool == 'htk':
+            with open(join(args.data_save_path, data_type, 'wav2htk.scp'), 'r') as f:
+                for line in f:
+                    line = line.strip()
+                    htk_path = line.split('  ')[1]
+                    speaker = basename(htk_path).split('.')[0]
+                    spk2audio[speaker] = htk_path
+        else:
+            with open(join(args.data_save_path, data_type, 'wav.scp'), 'r') as f:
+                for line in f:
+                    line = line.strip()
+                    speaker = line.split(' ')[0]
+                    wav_path = line.split(' ')[2]
+                    spk2audio[speaker] = wav_path
+
+        if args.tool == 'wav':
+            # Split WAV files per utterance
+            raise ValueError
+            # split_wav(wav_paths=wav_paths,
+            #           speaker_dict=speaker_dict_dict[data_type],
+            #           save_path=mkdir_join(feature_save_path, data_type))
+
+        else:
+            if data_type == 'train':
+                global_mean_male, global_std_male = None, None
+                global_mean_female, global_std_female = None, None
+            else:
+                # Load statistics over train dataset
+                global_mean_male = np.load(
+                    join(args.data_save_path, 'feature', args.tool, 'train/global_mean_male.npy'))
+                global_std_male = np.load(
+                    join(args.data_save_path, 'feature', args.tool, 'train/global_std_male.npy'))
+                global_mean_female = np.load(
+                    join(args.data_save_path, 'feature', args.tool, 'train/global_mean_female.npy'))
+                global_std_female = np.load(
+                    join(args.data_save_path, 'feature', args.tool, 'train/global_std_female.npy'))
+
+            read_audio(data_type=data_type,
+                       spk2audio=spk2audio,
+                       segment_dict=segment_dict,
+                       tool=args.tool,
+                       config=CONFIG,
+                       normalize=args.normalize,
+                       save_path=feature_save_path,
+                       global_mean_male=global_mean_male,
+                       global_std_male=global_std_male,
+                       global_mean_female=global_mean_female,
+                       global_std_female=global_std_female)
+
+
+def read_audio(data_type, spk2audio, segment_dict, tool, config, normalize,
+               save_path, global_mean_male=None, global_mean_female=None,
+               global_std_male=None, global_std_female=None, dtype=np.float32):
+    """Read HTK or WAV files.
+    Args:
+        data_type (string):
+        spk2audio (dict):
+        segment_dict (dict):
+            key (string) =>
+            value (dict) =>
+        tool (string): the tool to extract features,
+            htk or librosa or python_speech_features
+        config (dict): a configuration for feature extraction
+        normalize (string):
+            no => normalization will be not conducted
+            global => normalize input features by global mean & stddev over
+                      the training set per gender
+            speaker => normalize input features by mean & stddev per speaker
+            utterance => normalize input features by mean & stddev per utterancet
+                         data by mean & stddev per utterance
+        save_path (string): path to save npy files
+        global_mean_male (np.ndarray, optional): global mean of male over the
+            training set
+        global_std_male (np.ndarray, optional): global standard deviation of
+            male over the training set
+        global_mean_female (np.ndarray, optional): global mean of female over
+            the training set
+        global_std_female (np.ndarray, optional): global standard deviation of
+            female over the training set
+        dtype (optional): the type of data, default is np.float32
+    Returns:
+        global_mean_male (np.ndarray): global mean of male over the
+            training set
+        global_std_male (np.ndarray): global standard deviation of male
+            over the training set
+        global_mean_female (np.ndarray): global mean of female over the
+            training set
+        global_std_female (np.ndarray): global standard deviation of
+            female over the training set
+        frame_num_dict (dict):
+            key => utterance name
+            value => the number of frames
+    """
+    if data_type != 'train':
+        if global_mean_male is None or global_mean_female is None:
+            raise ValueError('Set mean & stddev computed in the training set.')
+    if normalize not in ['global', 'speaker', 'utterance', 'no']:
+        raise ValueError(
+            'normalize must be "utterance" or "speaker" or "global" or "no".')
+    if tool not in ['htk', 'python_speech_features', 'librosa']:
+        raise TypeError(
+            'tool must be "htk" or "python_speech_features" or "librosa".')
+
+    audio_paths_male, audio_paths_female = [], []
+    total_frame_num_male, total_frame_num_female = 0, 0
+    total_frame_num_dict = {}
+    speaker_mean_dict = {}
+
+    # NOTE: assume that speakers are different between sessions
+
+    # Loop 1: Computing global mean and statistics
+    if data_type == 'train' and normalize != 'no':
+        print('=====> Reading audio files...')
+        for i, speaker in enumerate(tqdm(segment_dict.keys())):
+            audio_path = spk2audio[speaker]
+
+            # Divide each audio file into utterances
+            _, features_utt_sum, speaker_mean, _, total_frame_num_speaker = segment(
+                audio_path,
+                speaker,
+                segment_dict[speaker],  # dict of utterances
+                is_training=True,
+                sil_duration=0,
+                tool=tool,
+                config=config)
+
+            if i == 0:
+                # Initialize global statistics
+                feature_dim = features_utt_sum.shape[0]
+                global_mean_male = np.zeros((feature_dim,), dtype=dtype)
+                global_mean_female = np.zeros(
+                    (feature_dim,), dtype=dtype)
+                global_std_male = np.zeros((feature_dim,), dtype=dtype)
+                global_std_female = np.zeros((feature_dim,), dtype=dtype)
+
+            # For computing global mean
+            if speaker[3] == 'M':
+                audio_paths_male.append(audio_path)
+                global_mean_male += features_utt_sum
+                total_frame_num_male += total_frame_num_speaker
+            elif speaker[3] == 'F':
+                audio_paths_female.append(audio_path)
+                global_mean_female += features_utt_sum
+                total_frame_num_female += total_frame_num_speaker
+            else:
+                raise ValueError
+
+            # For computing speaker mean & stddev
+            if normalize == 'speaker':
+                speaker_mean_dict[speaker] = speaker_mean
+                total_frame_num_dict[speaker] = total_frame_num_speaker
+                # NOTE: speaker mean is already computed
+
+        print('=====> Computing global mean & stddev...')
+        # Compute global mean per gender
+        global_mean_male /= total_frame_num_male
+        global_mean_female /= total_frame_num_female
+
+        for speaker in tqdm(segment_dict.keys()):
+            audio_path = spk2audio[speaker]
+
+            # Divide each audio into utterances
+            features_dict_speaker, _, _, _, _ = segment(
+                audio_path,
+                speaker,
+                segment_dict[speaker],
+                is_training=True,
+                sil_duration=0,
+                tool=tool,
+                config=config)
+
+            # For computing global stddev
+            if speaker[3] == 'M':
+                for features_utt in features_dict_speaker.values():
+                    global_std_male += np.sum(
+                        np.abs(features_utt - global_mean_male) ** 2, axis=0)
+            elif speaker[3] == 'F':
+                for features_utt in features_dict_speaker.values():
+                    global_std_female += np.sum(
+                        np.abs(features_utt - global_mean_female) ** 2, axis=0)
+            else:
+                raise ValueError
+
+        # Compute global stddev per gender
+        global_std_male = np.sqrt(
+            global_std_male / (total_frame_num_male - 1))
+        global_std_female = np.sqrt(
+            global_std_female / (total_frame_num_female - 1))
+
+        if save_path is not None:
+            # Save global mean & stddev per gender
+            np.save(join(save_path, 'global_mean_male.npy'),
+                    global_mean_male)
+            np.save(join(save_path, 'global_mean_female.npy'),
+                    global_mean_female)
+            np.save(join(save_path, 'global_std_male.npy'),
+                    global_std_male)
+            np.save(join(save_path, 'global_std_female.npy'),
+                    global_std_female)
+
+    # Loop 2: Normalization and Saving
+    print('=====> Normalization...')
+    frame_num_dict = {}
+    # sampPeriod, parmKind = None, None
+    for speaker in tqdm(segment_dict.keys()):
+        audio_path = spk2audio[speaker]
+
+        if normalize == 'speaker' and data_type == 'train':
+            speaker_mean = speaker_mean_dict[speaker]
+        else:
+            speaker_mean = None
+
+        # Divide each audio into utterances
+        features_dict_speaker, _, speaker_mean, speaker_std, _ = segment(
+            audio_path,
+            speaker,
+            segment_dict[speaker],
+            is_training=data_type == 'train',
+            sil_duration=0,
+            tool=tool,
+            config=config,
+            mean=speaker_mean)  # for compute speaker stddev
+        # NOTE: features_dict_speaker have been not normalized yet
+
+        for utt_idx, features_utt in features_dict_speaker.items():
+            if normalize == 'no':
+                pass
+            elif normalize == 'global' or not data_type == 'train':
+                # Normalize by mean & stddev over the training set per gender
+                if speaker[3] == 'M':
+                    features_utt -= global_mean_male
+                    features_utt /= global_std_male
+                elif speaker[3] == 'F':
+                    features_utt -= global_mean_female
+                    features_utt /= global_std_female
+                else:
+                    raise ValueError
+            elif normalize == 'speaker':
+                # Normalize by mean & stddev per speaker
+                features_utt = (features_utt - speaker_mean) / speaker_std
+            elif normalize == 'utterance':
+                # Normalize by mean & stddev per utterance
+                utt_mean = np.mean(features_utt, axis=0, dtype=dtype)
+                utt_std = np.std(features_utt, axis=0, dtype=dtype)
+                features_utt = (features_utt - utt_mean) / utt_std
+            else:
+                raise ValueError
+
+            frame_num_dict[speaker + '_' + utt_idx] = features_utt.shape[0]
+
+            if save_path is not None:
+                # Save input features
+                input_data_save_path = mkdir_join(
+                    save_path, speaker, speaker + '_' + utt_idx + '.npy')
+                np.save(input_data_save_path, features_utt)
+
+                # if sampPeriod is None:
+                #     _, sampPeriod, parmKind = read(audio_path)
+                # write(features_utt,
+                #       htk_path=mkdir_join(
+                #           save_path, speaker, speaker + '_' + utt_idx + '.htk'),
+                #       sampPeriod=sampPeriod,
+                #       parmKind=parmKind)
+
+    if save_path is not None:
+        # Save the frame number dictionary
+        with open(join(save_path, 'frame_num.pickle'), 'wb') as f:
+            pickle.dump(frame_num_dict, f)
+
+    return (global_mean_male, global_mean_female,
+            global_std_male, global_std_female, frame_num_dict)
+
+
+if __name__ == '__main__':
+    main()
---
+++ examples/csj/s5/run.sh
@@ -0,0 +1,294 @@
+#!/bin/bash
+
+. ./cmd.sh
+. ./path.sh
+set -e
+
+# . utils/parse_options.sh  # e.g. this parses the --stage option if supplied.
+
+if [ $# -ne 2 ]; then
+  echo "Error: set GPU number & config path." 1>&2
+  echo "Usage: ./run.sh path_to_config_file gpu_index" 1>&2
+  exit 1
+fi
+
+
+echo ============================================================================
+echo "                                   CSJ                                     "
+echo ============================================================================
+
+stage=1
+hierarchical_model=false
+
+### Set path to original data
+CSJDATATOP="/n/rd25/mimura/corpus/CSJ"
+#CSJDATATOP=/db/laputa1/$DATA_SAVEPATH/processed/public/CSJ ## CSJ database top directory.
+CSJVER=dvd  ## Set your CSJ format (dvd or usb).
+            ## Usage    :
+            ## Case DVD : We assume CSJ DVDs are copied in this directory with the names dvd1, dvd2,...,dvd17.
+            ##            Neccesary directory is dvd3 - dvd17.
+            ##            e.g. $ ls $CSJDATATOP(DVD) => 00README.txt dvd1 dvd2 ... dvd17
+            ##
+            ## Case USB : Neccesary directory is MORPH/SDB and WAV
+            ##            e.g. $ ls $CSJDATATOP(USB) => 00README.txt DOC MORPH ... WAV fileList.csv
+            ## Case merl :MERL setup. Neccesary directory is WAV and sdb
+
+### Select data size
+DATASIZE=subset
+# DATASIZE=aps
+# DATASIZE=fullset
+# DATASIZE=all
+
+### Set path to save dataset
+export DATA_SAVEPATH="/n/sd8/inaguma/corpus/csj/kaldi"/$DATASIZE
+
+### Set path to save the model
+MODEL_SAVEPATH="/n/sd8/inaguma/result/csj"
+
+### Select one tool to extract features (HTK is the fastest)
+# TOOL='kaldi'
+TOOL='htk'
+# TOOL='python_speech_features'
+# TOOL='librosa'
+# # TOOL='wav'
+
+### Configuration of feature extranction
+CHANNELS=80
+WINDOW=0.025
+SLIDE=0.01
+ENERGY=0
+DELTA=1
+DELTADELTA=1
+# NORMALIZE='global'
+NORMALIZE='speaker'
+# NORMALIZE='utterance'
+# NORMALIZE='no'
+# NOTE: normalize in [-1, 1] in case of WAV
+
+
+if [ $stage -le 0 ]; then
+  echo ============================================================================
+  echo "                           Data Preparation                               "
+  echo ============================================================================
+
+  if [ ! -e $DATA_SAVEPATH/csj-data/.done_make_all ]; then
+   echo "CSJ transcription file does not exist"
+   #local/csj_make_trans/csj_autorun.sh <RESOUCE_DIR> <MAKING_PLACE(no change)> || exit 1;
+   local/csj_make_trans/csj_autorun.sh $CSJDATATOP $DATA_SAVEPATH/csj-data $CSJVER || exit 1;
+  fi
+  wait
+
+  [ ! -e $DATA_SAVEPATH/csj-data/.done_make_all ]\
+      && echo "Not finished processing CSJ data" && exit 1;
+
+  rm -rf $DATA_SAVEPATH/local
+
+  # Prepare Corpus of Spontaneous Japanese (CSJ) data.
+  # Processing CSJ data to KALDI format based on switchboard recipe.
+  # local/csj_data_prep.sh <SPEECH_and_TRANSCRIPTION_DATA_DIRECTORY> [ <mode_number> ]
+  # mode_number can be 0, 1, 2, 3 (0=default using "Academic lecture" and "other" data,
+  #                                1=using "Academic lecture" data,
+  #                                2=using All data except for "dialog" data, 3=using All data )
+  if [ $DATASIZE = 'subset' ]; then
+    local/csj_data_prep.sh $DATA_SAVEPATH/csj-data || exit 1;  # subset (240h)
+  elif [ $DATASIZE = 'aps' ]; then
+    local/csj_data_prep.sh $DATA_SAVEPATH/csj-data 1 || exit 1;  # aps
+  elif [ $DATASIZE = 'fullset' ]; then
+    local/csj_data_prep.sh $DATA_SAVEPATH/csj-data 2 || exit 1;  # fullset (586h)
+  elif [ $DATASIZE = 'all' ]; then
+    local/csj_data_prep.sh $DATA_SAVEPATH/csj-data 3 || exit 1;  # all
+  fi
+
+  local/csj_prepare_dict.sh || exit 1;
+
+  # Use the first 4k sentences from training data as dev set. (39 speakers.)
+  # NOTE: when we trained the LM, we used the 1st 10k sentences as dev set,
+  # so the 1st 4k won't have been used in the LM training data.
+  # However, they will be in the lexicon, plus speakers may overlap,
+  # so it's still not quite equivalent to a test set.
+  utils/subset_data_dir.sh --first $DATA_SAVEPATH/train 4000 $DATA_SAVEPATH/dev || exit 1; # 6hr 31min
+  n=$[`cat $DATA_SAVEPATH/train/segments | wc -l` - 4000]
+  utils/subset_data_dir.sh --last $DATA_SAVEPATH/train $n $DATA_SAVEPATH/train_nodev || exit 1;
+
+  # Take the first 100k utterances (about half the data); we'll use
+  # this for later stages of training.
+  # utils/subset_data_dir.sh --first $DATA_SAVEPATH/train_nodev 100000 $DATA_SAVEPATH/train_100k || exit 1;
+  # utils/data/remove_dup_utts.sh 200 $DATA_SAVEPATH/train_100k $DATA_SAVEPATH/train_100k_tmp || exit 1;  # 147hr 6min
+  # rm -rf $DATA_SAVEPATH/train_100k
+  # mv $DATA_SAVEPATH/train_100k_tmp $DATA_SAVEPATH/train_100k || exit 1;
+
+  # Finally, the full training set:
+  rm -rf $DATA_SAVEPATH/train
+  utils/data/remove_dup_utts.sh 300 $DATA_SAVEPATH/train_nodev $DATA_SAVEPATH/train || exit 1;  # 233hr 36min
+  rm -rf $DATA_SAVEPATH/train_nodev
+
+  # Data preparation and formatting for evaluation set.
+  # CSJ has 3 types of evaluation data
+  #local/csj_eval_data_prep.sh <SPEECH_and_TRANSCRIPTION_DATA_DIRECTORY_ABOUT_EVALUATION_DATA> <EVAL_NUM>
+  for eval_num in eval1 eval2 eval3 ; do
+      local/csj_eval_data_prep.sh $DATA_SAVEPATH/csj-data/eval $eval_num || exit 1;
+  done
+
+  echo "Finish data preparation (stage: 0)."
+fi
+
+# Calculate the amount of utterance segmentations.
+# perl -ne 'split; $s+=($_[3]-$_[2]); END{$h=int($s/3600); $r=($s-$h*3600); $m=int($r/60); $r-=$m*60; printf "%.1f sec -- %d:%d:%.1f\n", $s, $h, $m, $r;}' $DATA_SAVEPATH/train/segments
+# perl -ne 'split; $s+=($_[3]-$_[2]); END{$h=int($s/3600); $r=($s-$h*3600); $m=int($r/60); $r-=$m*60; printf "%.1f sec -- %d:%d:%.1f\n", $s, $h, $m, $r;}' $DATA_SAVEPATH/eval1/segments
+# perl -ne 'split; $s+=($_[3]-$_[2]); END{$h=int($s/3600); $r=($s-$h*3600); $m=int($r/60); $r-=$m*60; printf "%.1f sec -- %d:%d:%.1f\n", $s, $h, $m, $r;}' $DATA_SAVEPATH/eval2/segments
+# perl -ne 'split; $s+=($_[3]-$_[2]); END{$h=int($s/3600); $r=($s-$h*3600); $m=int($r/60); $r-=$m*60; printf "%.1f sec -- %d:%d:%.1f\n", $s, $h, $m, $r;}' $DATA_SAVEPATH/eval3/segments
+
+
+if [ $stage -le 1 ]; then
+  echo ============================================================================
+  echo "                        Feature extranction                               "
+  echo ============================================================================
+
+  if [ $TOOL = 'kaldi' ]; then
+    for x in train dev eval1 eval2 eval3; do
+      steps/make_fbank.sh --nj 8 --cmd run.pl $DATA_SAVEPATH/$x $DATA_SAVEPATH/make_fbank/$x $DATA_SAVEPATH/fbank || exit 1;
+      steps/compute_cmvn_stats.sh $DATA_SAVEPATH/$x $DATA_SAVEPATH/make_fbank/$x $DATA_SAVEPATH/fbank || exit 1;
+      utils/fix_data_dir.sh $DATA_SAVEPATH/$x || exit 1;
+    done
+
+  elif [ $TOOL = 'htk' ]; then
+    # Make a config file to covert from wav to htk file
+    python local/make_htk_config.py \
+        --data_save_path $DATA_SAVEPATH \
+        --config_save_path ./conf/fbank_htk.conf \
+        --channels $CHANNELS \
+        --window $WINDOW \
+        --slide $SLIDE \
+        --energy $ENERGY \
+        --delta $DELTA \
+        --deltadelta $DELTADELTA || exit 1;
+
+    # Convert from wav to htk files
+    for data_type in train dev eval1 eval2 eval3 ; do
+      mkdir -p $DATA_SAVEPATH/htk
+      mkdir -p $DATA_SAVEPATH/htk/$data_type
+
+      if [ ! -e $DATA_SAVEPATH/htk/$data_type/.done_make_htk ]; then
+        $HCOPY -T 1 -C ./conf/fbank_htk.conf -S $DATA_SAVEPATH/$data_type/wav2htk.scp || exit 1;
+        touch $DATA_SAVEPATH/htk/$data_type/.done_make_htk
+      fi
+    done
+
+  else
+    if ! which sox >&/dev/null; then
+      echo "This script requires you to first install sox";
+      exit 1;
+    fi
+  fi
+
+  if [ ! -e $DATA_SAVEPATH/feature/$TOOL/.done_feature_extraction ]; then
+    python local/feature_extraction.py \
+      --data_save_path $DATA_SAVEPATH \
+      --tool $TOOL \
+      --normalize $NORMALIZE \
+      --channels $CHANNELS \
+      --window $WINDOW \
+      --slide $SLIDE \
+      --energy $ENERGY \
+      --delta $DELTA \
+      --deltadelta $DELTADELTA || exit 1;
+    touch $DATA_SAVEPATH/feature/$TOOL/.done_feature_extraction
+  fi
+
+  echo "Finish feature extranction (stage: 1)."
+fi
+
+
+if [ $stage -le 2 ]; then
+  echo ============================================================================
+  echo "                            Create dataset                                "
+  echo ============================================================================
+
+  echo "Finish creating dataset (stage: 2)."
+fi
+
+exit 1
+
+
+if [ $stage -le 3 ]; then
+  echo ============================================================================
+  echo "                             Training stage                               "
+  echo ============================================================================
+
+  config_path=$1
+  gpu_index=$2
+  filename=$(basename $config_path | awk -F. '{print $1}')
+
+  mkdir -p exp/training/log
+  mkdir -p $MODEL_SAVEPATH
+
+  if $hierarchical_model; then
+    # CUDA_VISIBLE_DEVICES=$gpu_index CUDA_LAUNCH_BLOCKING=1 \
+    # nohup $PYTHON exp/training/train_hierarchical.py \
+    #   --gpu $gpu_index \
+    #   --config_path $config_path \
+    #   --model_save_path $MODEL_SAVE_PATH > log/$filename".log" &
+
+    CUDA_VISIBLE_DEVICES=$gpu_index CUDA_LAUNCH_BLOCKING=1 \
+    $PYTHON exp/training/train_hierarchical.py \
+      --gpu $gpu_index \
+      --config_path $config_path \
+      --model_save_path $MODEL_SAVE_PATH
+
+  else
+    # CUDA_VISIBLE_DEVICES=$gpu_index CUDA_LAUNCH_BLOCKING=1 \
+    # nohup $PYTHON exp/training/train.py \
+    #   --gpu $gpu_index \
+    #   --config_path $config_path \
+    #   --model_save_path $MODEL_SAVEPATH > log/$filename".log" &
+
+    CUDA_VISIBLE_DEVICES=$gpu_index CUDA_LAUNCH_BLOCKING=1 \
+    $PYTHON exp/training/train.py \
+      --gpu $gpu_index \
+      --config_path $config_path \
+      --model_save_path $MODEL_SAVEPATH || exit 1;
+  fi
+
+  echo "Finish model training (stage: 3)."
+fi
+
+
+if [ $stage -le 4 ]; then
+  echo ============================================================================
+  echo "                             LM training                                 "
+  echo ============================================================================
+
+  echo "Finish LM training (stage: 4)."
+fi
+
+
+if [ $stage -le 5 ]; then
+  echo ============================================================================
+  echo "                              Rescoring                                   "
+  echo ============================================================================
+
+  echo "Finish rescoring (stage: 5)."
+fi
+
+
+echo "Done."
+
+
+# utils/prepare_lang.sh --num-sil-states 4 $DATA_SAVEPATH/local/dict_nosp "<unk>" $DATA_SAVEPATH/local/lang_nosp $DATA_SAVEPATH/lang_nosp
+
+# Now train the language models.
+# local/csj_train_lms.sh $DATA_SAVEPATH/local/train/text $DATA_SAVEPATH/local/dict_nosp/lexicon.txt $DATA_SAVEPATH/local/lm
+
+# We don't really need all these options for SRILM, since the LM training script
+# does some of the same processing (e.g. -subset -tolower)
+# srilm_opts="-subset -prune-lowprobs -unk -tolower -order 3"
+# LM=$DATA_SAVEPATH/local/lm/csj.o3g.kn.gz
+# utils/format_lm_sri.sh --srilm-opts "$srilm_opts" \
+#   $DATA_SAVEPATH/lang_nosp $LM $DATA_SAVEPATH/local/dict_nosp/lexicon.txt $DATA_SAVEPATH/lang_nosp_csj_tg
+
+
+# getting results (see RESULTS file)
+# for eval_num in eval1 eval2 eval3 $dev_set ; do
+#     echo "=== evaluation set $eval_num ===" ;
+#     for x in exp/{tri,dnn}*/decode_${eval_num}*; do [ -d $x ] && grep WER $x/wer_* | utils/best_wer.sh; done ;
+# done
