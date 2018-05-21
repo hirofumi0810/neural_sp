2e636a009bcd9de0bc28b21a0b2e48f9d1e98b29
hirofumi0810 <hiro.mhbc@gmail.com>
2018-05-21 11:40:27 +0900

    Turn on env.sh

--- examples/swbd/s5c/path.sh
+++ examples/swbd/s5c/path.sh
@@ -2,7 +2,7 @@
 if [ -z $KALDI_ROOT ]; then
   export KALDI_ROOT="/home/inaguma/tool/kaldi"
 fi
-# [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
+[ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
 
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
