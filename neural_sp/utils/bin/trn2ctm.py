ceb5d206ec39e0ac1b0eeef3eb7e38930d0b4bbb
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2019-01-22 21:19:58 +0900

    Fix trn2ctm.py

--- neural_sp/utils/bin/trn2ctm.py
+++ neural_sp/utils/bin/trn2ctm.py
@@ -15,27 +15,28 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('trn', type=str,
                     help='trn file')
-parser.add_argument('--stm', type=str,
-                    help='stm file')
+parser.add_argument('--stm', type=str, default='', nargs='?',
+                    help='stm file (for Switchboard corpus)')
 args = parser.parse_args()
 
 
 def main():
 
     stm_segments = {}
-    with open(args.stm, 'r') as f:
-        for line in f:
-            line = line.strip()
+    if args.stm:
+        with open(args.stm, 'r') as f:
+            for line in f:
+                line = line.strip()
 
-            if line[0] == ';':
-                continue
+                if line[0] == ';':
+                    continue
 
-            speaker = line.split()[0]
-            start_t = float(line.split()[3])
-            end_t = float(line.split()[4])
-            if speaker not in stm_segments.keys():
-                stm_segments[speaker] = {}
-            stm_segments[speaker][start_t] = end_t
+                speaker = line.split()[0]
+                start_t = float(line.split()[3])
+                end_t = float(line.split()[4])
+                if speaker not in stm_segments.keys():
+                    stm_segments[speaker] = {}
+                stm_segments[speaker][start_t] = end_t
 
     with open(args.trn, 'r') as f:
         for line in f:
@@ -43,27 +44,32 @@ def main():
             line = line.strip()
             words = line.split()[:-1]
             utt_id = line.split()[-1].replace('(', '').replace(')', '')
-            speaker = '_'.join(utt_id.split('-')[0].split('_')[:-1])
-            channel = utt_id.split('-')[0].split('_')[-1]
+            if args.stm:
+                speaker = '_'.join(utt_id.split('-')[0].split('_')[:-1])
+                channel = utt_id.split('-')[0].split('_')[-1]  # A or B
+            else:
+                speaker = utt_id.split('-')[0]
+                channel = '1'
             start_f, end_f = utt_id.split('-')[1:]
             start_t = round(int(start_f) / 100, 2)
-            # end_t = round(int(end_f) / 100, 2)
 
-            # Fix end time based on the stm file
-            if start_t in stm_segments[speaker].keys():
-                end_t = stm_segments[speaker][start_t]
+            if args.stm:
+                # Fix end time based on the stm file
+                if start_t in stm_segments[speaker].keys():
+                    end_t = stm_segments[speaker][start_t]
+                else:
+                    end_t = stm_segments[speaker][round(start_t + 0.01, 2)]
             else:
-                end_t = stm_segments[speaker][round(start_t + 0.01, 2)]
+                end_t = round(int(end_f) / 100, 2)
 
             duration_t = end_t - start_t
             if len(words) > 0:
                 duration_t /= len(words)
 
-            assert channel in ['A', 'B']
-            conf = 1.  # dummy
+            confidence = 1  # Nist-1 manner in the ROVER paper
 
             for w in words:
-                print('%s %s %.2f %.2f %s %.3f' % (speaker, channel, start_t, duration_t, w, conf))
+                print('%s %s %.2f %.2f %s %.3f' % (speaker, channel, start_t, duration_t, w, confidence))
                 start_t += duration_t
 
 
