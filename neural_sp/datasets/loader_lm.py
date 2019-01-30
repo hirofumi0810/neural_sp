3ab27a9245c1efecfeaea89b80b71fdab6783015
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2019-01-31 05:08:19 +0900

    Fix epoch_detail

--- neural_sp/datasets/loader_lm.py
+++ neural_sp/datasets/loader_lm.py
@@ -108,7 +108,7 @@ class Dataset(Base):
     @property
     def epoch_detail(self):
         # Floating point version of epoch
-        return self.epoch + (self.offset / len(self.concat_ids.reshape((-1,))))
+        return self.epoch + (float(self.offset * self.batch_size) / len(self))
 
     def __next__(self, batch_size=None):
         """Generate each mini-batch.
