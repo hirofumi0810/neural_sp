d5e4cd2ef105944a43525438cb0ab0426343ea16
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2019-04-25 00:58:08 +0900

    Change parameter name in the attention layer

--- neural_sp/models/seq2seq/decoders/attention.py
+++ neural_sp/models/seq2seq/decoders/attention.py
@@ -66,13 +66,13 @@ class AttentionMechanism(nn.Module):
             # NOTE: sequence-to-sequence without attetnion (use the last state as a context vector)
 
         elif attn_type == 'add':
-            self.w_enc = LinearND(key_dim, attn_dim)
-            self.w_dec = LinearND(query_dim, attn_dim, bias=False)
+            self.w_key = LinearND(key_dim, attn_dim)
+            self.w_query = LinearND(query_dim, attn_dim, bias=False)
             self.v = LinearND(attn_dim, 1, bias=False)
 
         elif attn_type == 'location':
-            self.w_enc = LinearND(key_dim, attn_dim)
-            self.w_dec = LinearND(query_dim, attn_dim, bias=False)
+            self.w_key = LinearND(key_dim, attn_dim)
+            self.w_query = LinearND(query_dim, attn_dim, bias=False)
             self.w_conv = LinearND(conv_out_channels, attn_dim, bias=False)
             self.conv = nn.Conv2d(in_channels=1,
                                   out_channels=conv_out_channels,
@@ -83,15 +83,15 @@ class AttentionMechanism(nn.Module):
             self.v = LinearND(attn_dim, 1, bias=False)
 
         elif attn_type == 'dot':
-            self.w_enc = LinearND(key_dim, attn_dim, bias=False)
-            self.w_dec = LinearND(query_dim, attn_dim, bias=False)
+            self.w_key = LinearND(key_dim, attn_dim, bias=False)
+            self.w_query = LinearND(query_dim, attn_dim, bias=False)
 
         elif attn_type == 'luong_dot':
             pass
             # NOTE: no additional parameters
 
         elif attn_type == 'luong_general':
-            self.w_enc = LinearND(key_dim, query_dim, bias=False)
+            self.w_key = LinearND(key_dim, query_dim, bias=False)
 
         elif attn_type == 'luong_concat':
             self.w = LinearND(key_dim + query_dim, attn_dim, bias=False)
@@ -126,7 +126,7 @@ class AttentionMechanism(nn.Module):
         # Pre-computation of encoder-side features for computing scores
         if self.key is None:
             if self.attn_type in ['add', 'location', 'dot', 'luong_general']:
-                self.key = self.w_enc(key)
+                self.key = self.w_key(key)
             else:
                 self.key = key
 
@@ -139,16 +139,16 @@ class AttentionMechanism(nn.Module):
 
         if self.attn_type == 'add':
             query = query.expand_as(torch.zeros((bs, key_len, query.size(2))))
-            e = self.v(F.tanh(self.key + self.w_dec(query))).squeeze(2)
+            e = self.v(F.tanh(self.key + self.w_query(query))).squeeze(2)
 
         elif self.attn_type == 'location':
             query = query.expand_as(torch.zeros((bs, key_len, query.size(2))))
             conv_feat = self.conv(aw.view(bs, 1, 1, key_len)).squeeze(2)  # `[B, conv_out_channels, key_len]`
             conv_feat = conv_feat.transpose(2, 1).contiguous()  # `[B, key_len, conv_out_channels]`
-            e = self.v(F.tanh(self.key + self.w_dec(query) + self.w_conv(conv_feat))).squeeze(2)
+            e = self.v(F.tanh(self.key + self.w_query(query) + self.w_conv(conv_feat))).squeeze(2)
 
         elif self.attn_type == 'dot':
-            e = torch.matmul(self.key, self.w_dec(query).transpose(-1, -2)).squeeze(2)
+            e = torch.matmul(self.key, self.w_query(query).transpose(-1, -2)).squeeze(2)
 
         elif self.attn_type == 'luong_dot':
             e = torch.matmul(self.key, query.transpose(-1, -2)).squeeze(2)
@@ -161,7 +161,7 @@ class AttentionMechanism(nn.Module):
             e = self.v(F.tanh(self.w(torch.cat([self.key, query], dim=-1)))).squeeze(2)
 
         if self.attn_type == 'no':
-            last_state = [k[key_lens[b] - 1] for b, k in enumerate(key)]
+            last_state = [key[b, key_lens[b] - 1] for b in range(bs)]
             cv = torch.stack(last_state, dim=0).unsqueeze(1)
         else:
             # Compute attention weights, context vector
