de9bee587e6b2ab706bf47f6a4aa98007cd3157a
hirofumi0810 <hiro.mhbc@gmail.com>
2019-08-09 01:39:08 -0700

    Update lr scheduler

--- neural_sp/trainers/lr_scheduler.py
+++ neural_sp/trainers/lr_scheduler.py
@@ -172,8 +172,9 @@ class LRScheduler(object):
 
         It contains an entry for every variable in self.__dict__ which
         is not the optimizer.
+
         """
-        return {key: value for key, value in self.__dict__.items() if key != 'optimizer'}
+        return {key: value for key, value in self.__dict__.items()}
 
     def load_state_dict(self, state_dict):
         """Loads the schedulers state.
@@ -181,6 +182,7 @@ class LRScheduler(object):
         Arguments:
             state_dict (dict): scheduler state. Should be an object returned
                 from a call to :meth:`state_dict`.
+
         """
         self.__dict__.update(state_dict)
 
