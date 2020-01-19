c1460d9ded700d424d035fdea9255d5966a0c779
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2020-01-20 05:53:43 +0900

    Update

--- neural_sp/models/modules/sync_bidir_multihead_attention.py
+++ neural_sp/models/modules/sync_bidir_multihead_attention.py
@@ -37,13 +37,15 @@ class MultiheadAttentionMechanism(nn.Module):
     """
 
     def __init__(self, kdim, qdim, adim, atype, dropout=0., n_heads=4, bias=True,
-                 param_init=''):
+                 param_init='', future_weight=0.1):
         super(MultiheadAttentionMechanism, self).__init__()
 
         self.atype = atype
         assert adim % n_heads == 0
         self.d_k = adim // n_heads
         self.n_heads = n_heads
+        self.scale = math.sqrt(self.d_k)
+        self.future_weight = future_weight
         self.reset()
 
         # attention dropout applied AFTER the softmax layer
@@ -110,8 +112,9 @@ class MultiheadAttentionMechanism(nn.Module):
             cache (bool): cache key, value, and mask
             trigger_point (IntTensor): dummy
         Returns:
-            cv (FloatTensor): `[B, qlen, vdim]`
-            aw (FloatTensor): `[B, n_heads, qlen, klen]`
+            cv_fwd (FloatTensor): `[B, qlen, vdim]`
+            cv_bwd (FloatTensor): `[B, qlen, vdim]`
+            aw_fwd_h (FloatTensor): `[B, n_heads, qlen, klen]`
 
         """
         bs, klen = key_fwd.size()[: 2]
@@ -134,21 +137,45 @@ class MultiheadAttentionMechanism(nn.Module):
 
         query_fwd = self.w_query(query_fwd).view(bs, -1, self.n_heads, self.d_k)
         query_fwd = query_fwd.transpose(2, 1).contiguous()  # `[B, n_heads, qlen, d_k]`
+        query_bwd = self.w_query(query_bwd).view(bs, -1, self.n_heads, self.d_k)
+        query_bwd = query_bwd.transpose(2, 1).contiguous()  # `[B, n_heads, qlen, d_k]`
 
         if self.atype == 'scaled_dot':
-            e = torch.matmul(query_fwd, self.key_fwd.transpose(3, 2)) / math.sqrt(self.d_k)
+            e_fwd_h = torch.matmul(query_fwd, self.key_fwd.transpose(3, 2)) / self.scale
+            e_fwd_f = torch.matmul(query_fwd, self.key_bwd.transpose(3, 2)) / self.scale
+            e_bwd_h = torch.matmul(query_bwd, self.key_bwd.transpose(3, 2)) / self.scale
+            e_bwd_f = torch.matmul(query_bwd, self.key_fwd.transpose(3, 2)) / self.scale
         elif self.atype == 'add':
-            e = torch.tanh(self.key_fwd.unsqueeze(2) + query_fwd.unsqueeze(3))
-            e = e.permute(0, 2, 3, 1, 4).contiguous().view(bs, qlen, klen, -1)
-            e = self.v(e).permute(0, 3, 1, 2)
+            raise NotImplementedError
+            e_fwd_h = torch.tanh(self.key_fwd.unsqueeze(2) + query_fwd.unsqueeze(3))
+            e_fwd_h = e_fwd_h.permute(0, 2, 3, 1, 4).contiguous().view(bs, qlen, klen, -1)
+            e_fwd_h = self.v(e_fwd_h).permute(0, 3, 1, 2)
 
         # Compute attention weights
         if self.mask is not None:
-            e = e.masked_fill_(self.mask == 0, NEG_INF)  # `[B, n_heads, qlen, klen]`
-        aw = torch.softmax(e, dim=-1)
-        aw = self.attn_dropout(aw)
-        cv = torch.matmul(aw, self.value_fwd)  # `[B, n_heads, qlen, d_k]`
-        cv = cv.transpose(2, 1).contiguous().view(bs, -1,  self.n_heads * self.d_k)
-        cv = self.w_out(cv)
+            e_fwd_h = e_fwd_h.masked_fill_(self.mask == 0, NEG_INF)  # `[B, n_heads, qlen, klen]`
+            e_fwd_f = e_fwd_f.masked_fill_(self.mask == 0, NEG_INF)  # `[B, n_heads, qlen, klen]`
+            e_bwd_h = e_bwd_h.masked_fill_(self.mask == 0, NEG_INF)  # `[B, n_heads, qlen, klen]`
+            e_bwd_f = e_bwd_f.masked_fill_(self.mask == 0, NEG_INF)  # `[B, n_heads, qlen, klen]`
+        aw_fwd_h = self.attn_dropout(torch.softmax(e_fwd_h, dim=-1))
+        aw_fwd_f = self.attn_dropout(torch.softmax(e_fwd_f, dim=-1))
+        aw_bwd_h = self.attn_dropout(torch.softmax(e_bwd_h, dim=-1))
+        aw_bwd_f = self.attn_dropout(torch.softmax(e_bwd_f, dim=-1))
 
-        return cv, aw
+        cv_fwd_h = torch.matmul(aw_fwd_h, self.value_fwd)  # `[B, n_heads, qlen, d_k]`
+        cv_fwd_f = torch.matmul(aw_fwd_f, self.value_bwd)  # `[B, n_heads, qlen, d_k]`
+        cv_bwd_h = torch.matmul(aw_bwd_h, self.value_bwd)  # `[B, n_heads, qlen, d_k]`
+        cv_bwd_f = torch.matmul(aw_bwd_f, self.value_fwd)  # `[B, n_heads, qlen, d_k]`
+
+        cvs = []
+        for cv in [cv_fwd_h, cv_fwd_f, cv_bwd_h, cv_bwd_f]:
+            cv = cv.transpose(2, 1).contiguous().view(bs, -1,  self.n_heads * self.d_k)
+            cvs.append(cv)
+        cvs = self.w_out(torch.cat(cvs, dim=1))
+
+        # merge history and future information
+        time = cv.size(1)
+        cv_fwd = cvs[:, :time // 4] + self.future_weight * torch.tanh(cvs[:, time // 4:time // 2])
+        cv_bwd = cvs[:, time // 2:time * 3 // 4] + self.future_weight * torch.tanh(cvs[:, time * 3 // 4:])
+
+        return cv_fwd, cv_bwd, aw_fwd_h
