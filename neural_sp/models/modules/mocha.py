0c1e3960985a91b3b8cec04cf5082950b439d54b
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2020-02-12 03:26:24 +0900

    Add MHA to beta in MoChA

--- neural_sp/models/modules/mocha.py
+++ neural_sp/models/modules/mocha.py
@@ -9,6 +9,7 @@
 # [reference]
 # https://github.com/j-min/MoChA-pytorch/blob/94b54a7fa13e4ac6dc255b509dd0febc8c0a0ee6/attention.py
 
+import logging
 import math
 import numpy as np
 import torch
@@ -19,20 +20,24 @@ from neural_sp.models.modules.causal_conv import CausalConv1d
 
 NEG_INF = float(np.finfo(np.float32).min)
 
+logger = logging.getLogger(__name__)
 
-class Energy(nn.Module):
+
+class MonotonicEnergy(nn.Module):
     def __init__(self, kdim, qdim, adim, atype,
-                 init_r=None, conv1d=False, conv_kernel_size=5):
-        """Energy function.
+                 init_r, conv1d=False, conv_kernel_size=5, param_init=''):
+        """Energy function for the monotonic attenion.
 
         Args:
             kdim (int): dimension of key
             qdim (int): dimension of quary
             adim (int): dimension of attention space
             atype (str): type of attention mechanism
+            n_heads (int): number of heads
             init_r (int): initial value for offset r
             conv1d (bool): use 1D causal convolution for energy calculation
             conv_kernel_size (int): kernel size for 1D convolution
+            param_init (str):
 
         """
         super().__init__()
@@ -45,24 +50,26 @@ class Energy(nn.Module):
         self.scale = math.sqrt(adim)
 
         self.w_key = nn.Linear(kdim, adim)
-        self.w_query = nn.Linear(qdim, adim, bias=False)
         if atype == 'add':
             self.v = nn.Linear(adim, 1, bias=False)
-
-        if init_r is not None:
-            # for alpha
-            self.r = nn.Parameter(torch.Tensor([init_r]))
-            if atype == 'add':
-                self.v = nn.utils.weight_norm(self.v, name='weight', dim=0)
-                # initialization
-                self.v.weight_g.data = torch.Tensor([1 / adim]).sqrt()
-            elif atype == 'scaled_dot':
-                self.w_query = nn.utils.weight_norm(self.w_query, name='weight', dim=0)
-                # initialization
-                self.w_query.weight_g.data = torch.Tensor([1 / adim]).sqrt()
+            self.w_query = nn.Linear(qdim, adim, bias=False)
+        elif atype == 'scaled_dot':
+            self.w_query = nn.Linear(qdim, adim)
         else:
-            # for beta
-            self.r = None
+            raise NotImplementedError(atype)
+
+        self.r = nn.Parameter(torch.Tensor([init_r]))
+        if atype == 'add':
+            self.v = nn.utils.weight_norm(self.v, name='weight', dim=0)
+            # initialization
+            self.v.weight_g.data = torch.Tensor([1 / adim]).sqrt()
+        elif atype == 'scaled_dot':
+            # self.w_query = nn.utils.weight_norm(self.w_query, name='weight', dim=0)
+            # initialization
+            # self.w_query.weight_g.data = torch.Tensor([1 / adim]).sqrt()
+            if param_init == 'xavier_uniform':
+                self.reset_parameters(True)
+            # TODO: debug weight normalization
 
         self.conv1d = None
         if conv1d:
@@ -72,12 +79,24 @@ class Energy(nn.Module):
                                        stride=1)
             # padding=(conv_kernel_size - 1) // 2
 
+    def reset_parameters(self, bias):
+        """Initialize parameters with Xavier uniform distribution."""
+        logger.info('===== Initialize %s with Xavier uniform distribution =====' % self.__class__.__name__)
+        # NOTE: see https://github.com/pytorch/fairseq/blob/master/fairseq/modules/multihead_attention.py
+        nn.init.xavier_uniform_(self.w_key.weight, gain=1 / math.sqrt(2))
+        # nn.init.xavier_uniform_(self.w_query.weight_v, gain=1 / math.sqrt(2))
+        nn.init.xavier_uniform_(self.w_query.weight, gain=1 / math.sqrt(2))
+        if bias:
+            # newly introduced
+            nn.init.constant_(self.w_key.bias, 0.)
+            nn.init.constant_(self.w_query.bias, 0.)
+
     def reset(self):
         self.key = None
         self.mask = None
 
     def forward(self, key, query, mask, cache=True):
-        """Compute energy.
+        """Compute monotonic energy.
 
         Args:
             key (FloatTensor): `[B, klen, kdim]`
@@ -85,7 +104,7 @@ class Energy(nn.Module):
             mask (ByteTensor): `[B, qlen, klen]`
             cache (bool): cache key and mask
         Return:
-            energy (FloatTensor): `[B, 1, klen]`
+            energy (FloatTensor): `[B, qlen, klen]`
 
         """
         bs, klen, kdim = key.size()
@@ -95,17 +114,14 @@ class Energy(nn.Module):
             # 1d conv
             if self.conv1d is not None:
                 key = torch.relu(self.conv1d(key))
-            self.key = self.w_key(key)  # `[B, klen, adim]`
+            self.key = self.w_key(key)
             self.mask = mask
 
         if self.atype == 'add':
-            energy = torch.relu(self.key + self.w_query(query))
-            energy = self.v(energy).transpose(2, 1)  # `[B, 1, klen]`
+            energy = torch.relu(self.key.squeeze(1) + self.w_query(query))
+            energy = self.v(energy).transpose(2, 1)  # `[B, qlen, klen]`
         elif self.atype == 'scaled_dot':
             energy = torch.matmul(self.w_query(query), self.key.transpose(2, 1)) / self.scale
-            # `[B, qlen, klen]`
-        else:
-            raise NotImplementedError(self.atype)
 
         if self.r is not None:
             energy = energy + self.r
@@ -114,10 +130,99 @@ class Energy(nn.Module):
         return energy
 
 
+class ChunkEnergy(nn.Module):
+    def __init__(self, kdim, qdim, adim, atype, n_heads=1, param_init=''):
+        """Energy function for the chunkwise attention.
+
+        Args:
+            kdim (int): dimension of key
+            qdim (int): dimension of quary
+            adim (int): dimension of attention space
+            atype (str): type of attention mechanism
+            n_heads (int): number of heads
+            param_init (str):
+
+        """
+        super().__init__()
+
+        self.key = None
+        self.mask = None
+
+        self.atype = atype
+        assert adim % n_heads == 0
+        self.d_k = adim // n_heads
+        self.n_heads = n_heads
+        self.scale = math.sqrt(adim)
+
+        self.w_key = nn.Linear(kdim, adim)
+        if atype == 'add':
+            self.w_query = nn.Linear(qdim, adim, bias=False)
+            self.v = nn.Linear(adim, 1, bias=False)
+        elif atype == 'scaled_dot':
+            self.w_query = nn.Linear(qdim, adim)
+            if param_init == 'xavier_uniform':
+                self.reset_parameters(True)
+        else:
+            raise NotImplementedError(atype)
+
+    def reset_parameters(self, bias):
+        """Initialize parameters with Xavier uniform distribution."""
+        logger.info('===== Initialize %s with Xavier uniform distribution =====' % self.__class__.__name__)
+        # NOTE: see https://github.com/pytorch/fairseq/blob/master/fairseq/modules/multihead_attention.py
+        nn.init.xavier_uniform_(self.w_key.weight, gain=1 / math.sqrt(2))
+        nn.init.xavier_uniform_(self.w_query.weight, gain=1 / math.sqrt(2))
+        if bias:
+            # newly introduced
+            nn.init.constant_(self.w_key.bias, 0.)
+            nn.init.constant_(self.w_query.bias, 0.)
+
+    def reset(self):
+        self.key = None
+        self.mask = None
+
+    def forward(self, key, query, mask, cache=True):
+        """Compute chunkwise energy.
+
+        Args:
+            key (FloatTensor): `[B, klen, kdim]`
+            query (FloatTensor): `[B, qlen, qdim]`
+            mask (ByteTensor): `[B, qlen, klen]`
+            cache (bool): cache key and mask
+        Return:
+            energy (FloatTensor): `[B, n_heads, qlen, klen]`
+
+        """
+        bs, klen, kdim = key.size()
+        qlen = query.size(1)
+
+        # Pre-computation of encoder-side features for computing scores
+        if self.key is None or not cache:
+            key = self.w_key(key).view(bs, -1, self.n_heads, self.d_k)
+            self.key = key.transpose(2, 1).contiguous()  # `[B, n_heads, klen, d_k]`
+            self.mask = mask.unsqueeze(1).repeat(
+                [1, self.n_heads, 1, 1]) if mask is not None else None  # `[B, n_heads, qlen, klen]`
+            if self.mask is not None:
+                assert self.mask.size() == (bs, self.n_heads, qlen, klen)
+
+        if self.atype == 'add':
+            assert self.n_heads == 1
+            energy = torch.relu(self.key.squeeze(1) + self.w_query(query))
+            energy = self.v(energy).transpose(2, 1)  # `[B, qlen, klen]`
+            energy = energy.unsqueeze(1)  # `[B, 1, qlen, klen]`
+        elif self.atype == 'scaled_dot':
+            query = self.w_query(query).view(bs, -1, self.n_heads, self.d_k)
+            query = query.transpose(2, 1).contiguous()  # `[B, n_heads, qlen, d_k]`
+            energy = torch.matmul(query, self.key.transpose(3, 2)) / self.scale
+
+        if self.mask is not None:
+            energy = energy.masked_fill_(self.mask == 0, NEG_INF)
+        return energy
+
+
 class MoChA(nn.Module):
     def __init__(self, kdim, qdim, adim, atype, chunk_size, n_heads=1,
-                 adaptive=False, conv1d=False,
-                 init_r=-4, noise_std=1.0, eps=1e-6, sharpening_factor=1.0):
+                 conv1d=False, init_r=-4, noise_std=1.0, eps=1e-6,
+                 sharpening_factor=1.0, param_init=''):
         """Monotonic chunk-wise attention.
 
             "Monotonic Chunkwise Attention" (ICLR 2018)
@@ -132,46 +237,52 @@ class MoChA(nn.Module):
             qdim (int): dimension of query
             adim: (int) dimension of the attention layer
             atype (str): type of attention mechanism
-            n_heads (int): number of heads
             chunk_size (int): size of chunk
-            adaptive (bool): adaptive MoChA
+            n_heads (int): number of heads
             conv1d (bool): apply 1d convolution for energy calculation
             init_r (int): initial value for parameter 'r' used for monotonic attention
             noise_std (float): standard deviation for input noise
             eps (float):
-            sharpening_factor (flaot): sharping factor for beta calculation
+            sharpening_factor (float): sharping factor for beta calculation
+            param_init (str):
 
         """
         super(MoChA, self).__init__()
 
         self.atype = atype
-        assert atype in ['add', 'scaled_dot']
-
         self.chunk_size = chunk_size
-        self.adaptive = adaptive
-        self.unconstrained = False
-        if chunk_size >= 100:
-            self.unconstrained = True
-        self.n_heads = 1
-        # regularization
+        self.n_heads = n_heads
         self.noise_std = noise_std
         self.eps = eps
         self.sharpening_factor = sharpening_factor
 
-        self.monotonic_energy = Energy(kdim, qdim, adim, atype, init_r, conv1d)
-        self.chunk_energy = Energy(kdim, qdim, adim, atype) if chunk_size > 1 else None
+        self.monotonic_energy = MonotonicEnergy(kdim, qdim, adim, atype, init_r, conv1d,
+                                                param_init=param_init)
+        self.chunk_energy = ChunkEnergy(kdim, qdim, adim, atype,
+                                        n_heads, param_init) if chunk_size > 1 else None
+        if n_heads > 1:
+            self.w_value = nn.Linear(kdim, adim)
+            self.w_out = nn.Linear(adim, kdim)
+            if param_init == 'xavier_uniform':
+                self.reset_parameters(True)
 
-        self.chunk_len_energy = None
-        if adaptive:
-            assert chunk_size > 1
-            self.chunk_len_energy = Energy(kdim, qdim, adim, atype)
+    def reset_parameters(self, bias):
+        """Initialize parameters with Xavier uniform distribution."""
+        logger.info('===== Initialize %s with Xavier uniform distribution =====' % self.__class__.__name__)
+        # NOTE: see https://github.com/pytorch/fairseq/blob/master/fairseq/modules/multihead_attention.py
+        nn.init.xavier_uniform_(self.w_value.weight, gain=1 / math.sqrt(2))
+        if bias:
+            # newly introduced
+            nn.init.constant_(self.w_value.bias, 0.)
+
+        nn.init.xavier_uniform_(self.w_out.weight)
+        if bias:
+            nn.init.constant_(self.w_out.bias, 0.)
 
     def reset(self):
         self.monotonic_energy.reset()
         if self.chunk_size > 1:
             self.chunk_energy.reset()
-            if self.adaptive:
-                self.chunk_len_energy.reset()
 
     def forward(self, key, value, query, mask=None, aw_prev=None,
                 mode='hard', cache=True, trigger_point=None):
@@ -256,23 +367,22 @@ class MoChA(nn.Module):
         # Compute chunk energy
         beta = None
         if self.chunk_size > 1:
-            e_chunk = self.chunk_energy(key, query, mask, cache=cache)  # `[B, qlen, ken]`
-            if self.adaptive and self.atype == 'add':
-                e_chunk_len = self.chunk_len_energy(key, query, mask, cache=cache)
-                if self.unconstrained:
-                    chunk_len_dist = torch.exp(e_chunk_len)
-                else:
-                    chunk_len_dist = self.chunk_size * torch.sigmoid(e_chunk_len)
-                # avoid zero length
-                chunk_len_dist = chunk_len_dist.int() + 1
-                beta = efficient_adaptive_chunkwise_attention(
-                    alpha, e_chunk, chunk_len_dist, self.sharpening_factor)
-            else:
-                beta = efficient_chunkwise_attention(
-                    alpha, e_chunk, self.chunk_size, self.sharpening_factor)  # `[B, qlen, klen]`
+            e_chunk = self.chunk_energy(key, query, mask, cache=cache)  # `[B, n_heads, qlen, ken]`
+            beta = efficient_chunkwise_attention(
+                alpha, e_chunk, self.chunk_size, self.sharpening_factor)  # `[B, n_heads, qlen, klen]`
 
         # Compute context vector
-        cv = torch.bmm(beta if self.chunk_size > 1 else alpha, value)
+        if self.chunk_size == 1:
+            cv = torch.bmm(alpha, value)
+        else:
+            if self.n_heads > 1:
+                value = self.w_value(value).view(bs, -1, self.n_heads, self.d_k)
+                value = value.transpose(2, 1).contiguous()  # `[B, n_heads, klen, d_k]`
+                cv = torch.matmul(beta, value)  # `[B, n_heads, qlen, d_k]`
+                cv = cv.transpose(2, 1).contiguous().view(bs, -1,  self.n_heads * self.d_k)
+                cv = self.w_out(cv)
+            else:
+                cv = torch.bmm(beta.squeeze(1), value)
 
         return cv, alpha.transpose(2, 1)
 
@@ -322,23 +432,22 @@ def moving_sum(x, back, forward):
     """Compute the moving sum of x over a chunk_size with the provided bounds.
 
     Args:
-        x (FloatTensor): `[B, qlen, klen]`
+        x (FloatTensor): `[B, n_heads, qlen, klen]`
         back (int):
         forward (int):
 
     Returns:
-        x_sum (FloatTensor): `[B, qlen, klen]`
+        x_sum (FloatTensor): `[B, n_heads, qlen, klen]`
 
     """
     # Moving sum is computed as a carefully-padded 1D convolution with ones
-    x_padded = F.pad(x, pad=[back, forward])  # `[B, qlen, back + T + forward]`
+    x_padded = F.pad(x, pad=[back, forward])  # `[B, n_heads, qlen, back + T + forward]`
     # Construct filters
-    filters = x.new_ones(1, x_padded.size(1), back + forward + 1)
+    filters = x.new_ones(1, x_padded.size(1), x_padded.size(2), back + forward + 1)
     x_sum = F.conv1d(x_padded, filters)
     return x_sum
 
 
-# TODO: multi-head attention
 def efficient_chunkwise_attention(alpha, e, chunk_size, sharpening_factor=1.):
     """Compute chunkwise attention distribution efficiently by clipping logits.
 
@@ -351,44 +460,13 @@ def efficient_chunkwise_attention(alpha, e, chunk_size, sharpening_factor=1.):
 
     """
     # Shift logits to avoid overflow
-    e -= torch.max(e, dim=2, keepdim=True)[0]
+    e -= torch.max(e, dim=3, keepdim=True)[0]
     # Limit the range for numerical stability
     softmax_exp = torch.clamp(torch.exp(e), min=1e-5)
     # Compute chunkwise softmax denominators
     softmax_denominators = moving_sum(softmax_exp,
                                       back=chunk_size - 1, forward=0)
     # Compute \beta_{i, :}. emit_probs are \alpha_{i, :}.
-    beta = softmax_exp * moving_sum(alpha * sharpening_factor / softmax_denominators,
+    beta = softmax_exp * moving_sum(alpha.unsqueeze(1) * sharpening_factor / softmax_denominators,
                                     back=0, forward=chunk_size - 1)
-    return beta
-
-
-def efficient_adaptive_chunkwise_attention(alpha, e, chunk_len_dist, sharpening_factor=1.):
-    """Compute adaptive chunkwise attention distribution efficiently by clipping logits.
-
-    Args:
-        alpha (FloatTensor): `[B, qlen, klen]`
-        e (FloatTensor): `[B, qlen, klen]`
-        chunk_len_dist (IntTensor): `[B, klen]`
-    Return
-        beta (FloatTensor): `[B, qlen, klen]`
-
-    """
-    # Shift logits to avoid overflow
-    e -= torch.max(e, dim=2, keepdim=True)[0]
-    # Limit the range for numerical stability
-    softmax_exp = torch.clamp(torch.exp(e), min=1e-5)
-    # Compute chunkwise softmax denominators
-    boundary = torch.argmax(alpha, dim=1)
-    bs = alpha.size(0)
-    softmax_denominators = [moving_sum(
-        softmax_exp[b:b + 1],
-        back=chunk_len_dist[b, boundary[b]] - 1, forward=0)
-        for b in range(bs)]
-    # Compute \beta_{i, :}. emit_probs are \alpha_{i, :}.
-    beta = [softmax_exp[b:b + 1] * moving_sum(
-        alpha[b:b + 1] * sharpening_factor / softmax_denominators[b],
-        back=0, forward=chunk_len_dist[b, boundary[b]] - 1)
-        for b in range(bs)]
-    beta = torch.cat(beta, dim=0)
     return beta
