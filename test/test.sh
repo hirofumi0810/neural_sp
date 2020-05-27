1e5574ca64314c351d8231ecd00107bc3071d0b7
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2020-05-27 18:40:18 +0900

    Update test

--- test/test.sh
+++ test/test.sh
@@ -6,12 +6,12 @@ pytest ./test/encoders/test_conv_encoder.py
 pytest ./test/encoders/test_rnn_encoder.py
 pytest ./test/encoders/test_transformer_encoder.py
 # # decoder
-pytest ./test/test_las_decoder.py
-pytest ./test/test_transformer_decoder.py
-pytest ./test/test_rnn_transducer_decoder.py
+pytest ./test/decoders/test_las_decoder.py
+pytest ./test/decoders/test_transformer_decoder.py
+pytest ./test/decoders/test_rnn_transducer_decoder.py
 # # LM
-pytest ./test/test_rnnlm.py
-pytest ./test/test_transformerlm.py
-pytest ./test/test_transformer_xl_lm.py
+pytest ./test/lm/test_rnnlm.py
+pytest ./test/lm/test_transformerlm.py
+pytest ./test/lm/test_transformer_xl_lm.py
 # modules
 pytest ./test/modules/test_mocha.py
