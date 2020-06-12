c7a0ed4a9a0a7a1cdff6bbe881f5c681c3d51a66
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2020-06-12 10:31:58 +0900

    Add conformer

---
+++ neural_sp/models/modules/conformer_convolution.py
@@ -0,0 +1,93 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright 2020 Kyoto University (Hirofumi Inaguma)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+"""Convolution block for Conformer encoder."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import logging
+import torch.nn as nn
+import torch.nn.functional as F
+
+from neural_sp.models.modules.initialization import init_with_xavier_uniform
+from neural_sp.models.modules.swish import Swish
+
+logger = logging.getLogger(__name__)
+
+
+class ConformerConvBlock(nn.Module):
+    """A single convolution block for the Conformer encoder.
+
+    Args:
+        d_model (int): hidden dimension
+        kernel_size (int): kernel size for depthwise convolution
+        param_init (str): parameter initialization method
+
+    """
+
+    def __init__(self, d_model, kernel_size, param_init):
+        super(ConformerConvBlock, self).__init__()
+
+        self.d_model = d_model
+        assert kernel_size % 2 == 1
+
+        self.pointwise_conv1 = nn.Conv1d(in_channels=d_model,
+                                         out_channels=d_model * 2,  # for GLU
+                                         kernel_size=1,
+                                         stride=1,
+                                         padding=0)
+        self.depthwise_conv = nn.Conv1d(in_channels=d_model,
+                                        out_channels=d_model,
+                                        kernel_size=kernel_size,
+                                        stride=1,
+                                        # padding=kernel_size // 2 - 1,
+                                        padding=kernel_size // 2,
+                                        groups=d_model)  # depthwise
+        self.batch_norm = nn.BatchNorm1d(d_model)
+        self.activation = Swish()
+        self.pointwise_conv2 = nn.Conv1d(in_channels=d_model,
+                                         out_channels=d_model,
+                                         kernel_size=1,
+                                         stride=1,
+                                         padding=0)
+
+        if param_init == 'xavier_uniform':
+            self.reset_parameters()
+
+    def reset_parameters(self):
+        """Initialize parameters with Xavier uniform distribution."""
+        logger.info('===== Initialize %s with Xavier uniform distribution =====' % self.__class__.__name__)
+        for layer in [self.pointwise_conv1, self.pointwise_conv2, self.depthwise_conv]:
+            for n, p in layer.named_parameters():
+                init_with_xavier_uniform(n, p)
+
+    def forward(self, xs):
+        """Forward pass.
+
+        Args:
+            xs (FloatTensor): `[B, T, d_model]`
+        Returns:
+            xs (FloatTensor): `[B, T, d_model]`
+
+        """
+        B, T, d_model = xs.size()
+        assert d_model == self.d_model
+
+        xs = xs.transpose(2, 1).contiguous()  # `[B, C, T]`
+        xs = self.pointwise_conv1(xs)  # `[B, 2 * C, T]`
+        xs = xs.transpose(2, 1)  # `[B, T, 2 * C]`
+        xs = F.glu(xs)  # `[B, T, C]`
+        xs = xs.transpose(2, 1).contiguous()  # `[B, C, T]`
+        xs = self.depthwise_conv(xs)  # `[B, C, T]`
+
+        xs = self.batch_norm(xs)
+        xs = self.activation(xs)
+        xs = self.pointwise_conv2(xs)  # `[B, C, T]`
+
+        xs = xs.transpose(2, 1).contiguous()  # `[B, T, C]`
+        return xs
--- neural_sp/models/seq2seq/encoders/build.py
+++ neural_sp/models/seq2seq/encoders/build.py
@@ -73,6 +73,44 @@ def build_encoder(args):
             chunk_size_current=args.lc_chunk_size_current,
             chunk_size_right=args.lc_chunk_size_right)
 
+    elif 'conformer' in args.enc_type:
+        from neural_sp.models.seq2seq.encoders.conformer import ConformerEncoder
+        encoder = ConformerEncoder(
+            input_dim=args.input_dim if args.input_type == 'speech' else args.emb_dim,
+            enc_type=args.enc_type,
+            n_heads=args.transformer_n_heads,
+            kernel_size=args.conformer_kernel_size,
+            n_layers=args.enc_n_layers,
+            n_layers_sub1=args.enc_n_layers_sub1,
+            n_layers_sub2=args.enc_n_layers_sub2,
+            d_model=args.transformer_d_model,
+            d_ff=args.transformer_d_model * 4,
+            ffn_bottleneck_dim=getattr(args, 'transformer_ffn_bottleneck_dim', 0),
+            last_proj_dim=args.transformer_d_model if 'transformer' in args.dec_type else 0,
+            pe_type=args.transformer_enc_pe_type,
+            layer_norm_eps=args.transformer_layer_norm_eps,
+            ffn_activation='swish',
+            dropout_in=args.dropout_in,
+            dropout=args.dropout_enc,
+            dropout_att=args.dropout_att,
+            dropout_layer=args.dropout_enc_layer,
+            n_stacks=args.n_stacks,
+            n_splices=args.n_splices,
+            conv_in_channel=args.conv_in_channel,
+            conv_channels=args.conv_channels,
+            conv_kernel_sizes=args.conv_kernel_sizes,
+            conv_strides=args.conv_strides,
+            conv_poolings=args.conv_poolings,
+            conv_batch_norm=args.conv_batch_norm,
+            conv_layer_norm=args.conv_layer_norm,
+            conv_bottleneck_dim=args.conv_bottleneck_dim,
+            conv_param_init=args.param_init,
+            task_specific_layer=args.task_specific_layer,
+            param_init=args.transformer_param_init,
+            chunk_size_left=args.lc_chunk_size_left,
+            chunk_size_current=args.lc_chunk_size_current,
+            chunk_size_right=args.lc_chunk_size_right)
+
     else:
         from neural_sp.models.seq2seq.encoders.rnn import RNNEncoder
         encoder = RNNEncoder(
