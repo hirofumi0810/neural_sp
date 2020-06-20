725e372da2eb12486ddf0de0e9c4cc9e2c0fdeeb
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2020-06-20 22:29:32 +0900

    Update test

--- test/encoders/test_rnn_encoder_streaming_chunkwise.py
+++ test/encoders/test_rnn_encoder_streaming_chunkwise.py
@@ -40,10 +40,7 @@ def make_args(**kwargs):
         task_specific_layer=False,
         param_init=0.1,
         chunk_size_left=-1,
-        chunk_size_current=-1,
         chunk_size_right=-1,
-        streaming_type='carry_over',
-        latency_controllable=False
     )
     args.update(kwargs)
     return args
@@ -74,6 +71,11 @@ def make_args(**kwargs):
           'conv_channels': "32_32_32", 'conv_kernel_sizes': "(3,3)_(3,3)_(3,3)",
           'conv_strides': "(1,1)_(1,1)_(1,1)", 'conv_poolings': "(2, 2)_(2, 2)_(2, 2)",
           'chunk_size_left': 64, 'chunk_size_right': 32}),
+        # no CNN, frame stacking
+        ({'rnn_type': 'blstm', 'n_stacks': 4,
+          'chunk_size_left': 20, 'chunk_size_right': 20}),
+        ({'rnn_type': 'blstm', 'n_stacks': 4,
+          'chunk_size_left': 32, 'chunk_size_right': 16}),
     ]
 )
 def test_forward_streaming_chunkwise(args):
@@ -85,19 +87,12 @@ def test_forward_streaming_chunkwise(args):
     device_id = -1
     module = importlib.import_module('neural_sp.models.seq2seq.encoders.rnn')
     enc = module.RNNEncoder(**args)
-    factor = enc.subsampling_factor
 
-    hop_size = args['chunk_size_left']
-    assert hop_size % factor == 0
-    n_frame_lookback = 0
-    factor_tmp = factor
-    while True:
-        n_frame_lookback += factor_tmp
-        factor_tmp //= 2
-        if factor_tmp < 2:
-            break
-    n_frame_lookahead = n_frame_lookback
-    print(n_frame_lookahead)
+    factor = enc.subsampling_factor
+    N_l = enc.chunk_size_left
+    N_r = enc.chunk_size_right
+    lookback = enc.conv.n_frames_context if enc.conv is not None else 0
+    lookahead = enc.conv.n_frames_context if enc.conv is not None else 0
 
     module_frame_stack = importlib.import_module('neural_sp.models.seq2seq.frontends.frame_stacking')
 
@@ -113,9 +108,11 @@ def test_forward_streaming_chunkwise(args):
             if args['n_stacks'] > 1:
                 xs = [module_frame_stack.stack_frame(x, args['n_stacks'], args['n_stacks']) for x in xs]
                 xlens = xlens // args['n_stacks'] if xmax % args['n_stacks'] == 0 else xlens // args['n_stacks'] + 1
+                xmax = xlens.max().item()
 
             # all inputs
             xs_pad = pad_list([np2tensor(x, device_id).float() for x in xs], 0.)
+
             enc_out_dict = enc(xs_pad, xlens, task='all')
             assert enc_out_dict['ys']['xs'].size(0) == batch_size
             assert enc_out_dict['ys']['xs'].size(1) == enc_out_dict['ys']['xlens'][0]
@@ -124,46 +121,35 @@ def test_forward_streaming_chunkwise(args):
 
             # chunk by chunk encoding
             eouts_stream = []
-            if 'conv' in args['rnn_type']:
-                n_chunks = xmax // hop_size
-                if xmax % hop_size != 0:
-                    n_chunks += 1
-                j = 0  # time offset for input
-                j_out = 0  # time offset for encoder output
-                for i_chunk in range(n_chunks):
-                    start = j - n_frame_lookback
-                    end = (j + hop_size) + n_frame_lookahead
-                    xs_pad_stream = pad_list(
-                        [np2tensor(x[max(0, start):end], device_id).float() for x in xs], 0.)
+            n_chunks = xmax // N_l
+            if xmax % N_l != 0:
+                n_chunks += 1
+            j = 0  # time offset for input
+            j_out = 0  # time offset for encoder output
+            for i_chunk in range(n_chunks):
+                start = j - lookback
+                end = (j + N_l + N_r) + lookahead
+                xs_pad_stream = pad_list(
+                    [np2tensor(x[max(0, start):end], device_id).float() for x in xs], 0.)
 
-                    xlens_stream = torch.IntTensor([xs_pad_stream.size(1) for x in xs])
-                    enc_out_dict_stream = enc(xs_pad_stream, xlens_stream, task='all',
-                                              streaming=True,
-                                              lookback=start > 0,
-                                              lookahead=end < xmax - 1)
+                xlens_stream = torch.IntTensor([xs_pad_stream.size(1) for x in xs])
+                enc_out_dict_stream = enc(xs_pad_stream, xlens_stream, task='all',
+                                          streaming=True,
+                                          lookback=start > 0,
+                                          lookahead=end < xmax - 1)
 
-                    b = enc_out_dict['ys']['xs'][:, j_out:j_out + (hop_size // factor)]
-                    eouts_stream.append(b)
+                a = enc_out_dict['ys']['xs'][:, j_out:j_out + (N_l // factor)]
+                b = enc_out_dict_stream['ys']['xs'][:, :a.size(1)]
+                print(torch.equal(a, b))
+                print(a.size())
+                print(b.size())
+                eouts_stream.append(b)
 
-                    j += hop_size
-                    j_out += (hop_size // factor)
-                    if j > xmax:
-                        break
+                j += N_l
+                j_out += (N_l // factor)
+                if j > xmax:
+                    break
 
-                eouts_stream = torch.cat(eouts_stream, dim=1)
-                assert enc_out_dict['ys']['xs'].size() == eouts_stream.size()
-                assert torch.equal(enc_out_dict['ys']['xs'], eouts_stream)
-
-            else:
-                # frame stacking
-                xmax_stack = xs_pad.size(1)
-                for j in range(xmax_stack):
-                    xs_pad_stream = pad_list(
-                        [np2tensor(x[j:j + 1], device_id).float() for x in xs], 0.)
-                    xlens_stream = torch.IntTensor([args['n_stacks'] for x in xs])
-                    enc_out_dict_stream = enc(xs_pad_stream, xlens_stream, task='all', streaming=True)
-                    eouts_stream.append(enc_out_dict_stream['ys']['xs'])
-
-                eouts_stream = torch.cat(eouts_stream, dim=1)
-                assert enc_out_dict['ys']['xs'].size() == eouts_stream.size()
-                assert torch.equal(enc_out_dict['ys']['xs'], eouts_stream)
+            eouts_stream = torch.cat(eouts_stream, dim=1)
+            assert enc_out_dict['ys']['xs'].size() == eouts_stream.size()
+            assert torch.equal(enc_out_dict['ys']['xs'], eouts_stream)
