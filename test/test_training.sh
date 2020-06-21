bfba1ed2712c3e7ad05acfc011a14a7d50bb974a
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2020-06-21 18:55:55 +0900

    Add test for streaming

--- test/test_training.sh
+++ test/test_training.sh
@@ -12,5 +12,7 @@ cd ./examples/ci_test || exit 1;
 ./run.sh --stage 4 --conf conf/asr/blstm_las.yaml --conf2 conf/data/spec_augment.yaml || exit 1;
 ./run.sh --stage 4 --conf conf/asr/blstm_las.yaml --conf2 conf/data/adaptive_spec_augment.yaml || exit 1;
 ./run.sh --stage 4 --conf conf/asr/blstm_transducer.yaml || exit 1;
+./run.sh --stage 4 --conf conf/asr/lcblstm_mocha_chunk4040.yaml || exit 1;
 ./run.sh --stage 4 --conf conf/asr/transformer.yaml || exit 1;
+./run.sh --stage 4 --conf conf/asr/lc_transformer_mma_mono4H_chunk4H_chunk16_from4L_headdrop0.5_64_128_64.yaml || exit 1;
 ./run.sh --stage 4 --conf conf/asr/conformer.yaml || exit 1;
