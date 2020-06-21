76e27cd86d7852b1786929ba7101900be4b1117c
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2020-06-21 22:27:53 +0900

    Minor fix

--- examples/ci_test/local/download_sample.sh
+++ examples/ci_test/local/download_sample.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 # install TIMIT samples (publicly available)
-mkdir -p ./sample
-wget --no-check-certificate -P ./sample https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.phn
-wget --no-check-certificate -P ./sample https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.txt
-wget --no-check-certificate -P ./sample https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.wav
-wget --no-check-certificate -P ./sample https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.wrd
+mkdir -p $(pwd)/sample
+wget --no-check-certificate -P $(pwd)/sample https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.phn
+wget --no-check-certificate -P $(pwd)/sample https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.txt
+wget --no-check-certificate -P $(pwd)/sample https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.wav
+wget --no-check-certificate -P $(pwd)/sample https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.wrd
