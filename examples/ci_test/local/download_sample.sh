58e8b53a0f9642ed4858ab76c68a0e91a17eb3ff
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2020-06-21 18:14:27 +0900

    Fix wget

--- examples/ci_test/local/download_sample.sh
+++ examples/ci_test/local/download_sample.sh
@@ -2,7 +2,7 @@
 
 # install TIMIT samples (publicly available)
 mkdir -p ./sample
-wget -P ./sample https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.phn
-wget -P ./sample https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.txt
-wget -P ./sample https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.wav
-wget -P ./sample https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.wrd
+wget --no-check-certificate -P ./sample https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.phn
+wget --no-check-certificate -P ./sample https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.txt
+wget --no-check-certificate -P ./sample https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.wav
+wget --no-check-certificate -P ./sample https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.wrd
