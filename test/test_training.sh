ef9222575ff4e35c5f0aeb20d1d32fd44339ada5
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2020-06-22 19:24:56 +0900

    Add test

--- test/test_training.sh
+++ test/test_training.sh
@@ -11,8 +11,10 @@ cd ./examples/ci_test || exit 1;
 ./run.sh --stage 4 --conf conf/asr/blstm_las.yaml || exit 1;
 ./run.sh --stage 4 --conf conf/asr/blstm_las.yaml --conf2 conf/data/spec_augment.yaml || exit 1;
 ./run.sh --stage 4 --conf conf/asr/blstm_las.yaml --conf2 conf/data/adaptive_spec_augment.yaml || exit 1;
+./run.sh --stage 4 --conf conf/asr/blstm_ctc.yaml || exit 1;
 ./run.sh --stage 4 --conf conf/asr/blstm_transducer.yaml || exit 1;
 ./run.sh --stage 4 --conf conf/asr/transformer.yaml || exit 1;
+./run.sh --stage 4 --conf conf/asr/transformer_ctc.yaml || exit 1;
 ./run.sh --stage 4 --conf conf/asr/conformer.yaml || exit 1;
 
 # streaming ASR
