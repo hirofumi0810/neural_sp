321502a053407b478529634b90aee7b0f4bd3868
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2020-06-28 22:58:53 +0900

    Remove __future__

--- neural_sp/models/modules/positional_embedding.py
+++ neural_sp/models/modules/positional_embedding.py
@@ -6,10 +6,6 @@
 
 """Positional Embeddings."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import copy
 import logging
 import math
