0373ae187752e90e36ebcbafec4058dff2d48593
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2020-07-06 11:12:57 +0900

    Update subsampling layer

--- neural_sp/models/seq2seq/encoders/subsampling.py
+++ neural_sp/models/seq2seq/encoders/subsampling.py
@@ -16,12 +16,12 @@ from neural_sp.models.seq2seq.encoders.conv import update_lens_1d
 class ConcatSubsampler(nn.Module):
     """Subsample by concatenating successive input frames."""
 
-    def __init__(self, factor, n_units):
+    def __init__(self, subsampling_factor, n_units):
         super(ConcatSubsampler, self).__init__()
 
-        self.factor = factor
-        if factor > 1:
-            self.proj = nn.Linear(n_units * factor, n_units)
+        self.subsampling_factor = subsampling_factor
+        if subsampling_factor > 1:
+            self.proj = nn.Linear(n_units * subsampling_factor, n_units)
 
     def forward(self, xs, xlens):
         """Forward pass.
@@ -34,17 +34,17 @@ class ConcatSubsampler(nn.Module):
             xlens (IntTensor): `[B]` (on CPU)
 
         """
-        if self.factor == 1:
+        if self.subsampling_factor == 1:
             return xs, xlens
 
         xs = xs.transpose(1, 0).contiguous()
-        xs = [torch.cat([xs[t - r:t - r + 1] for r in range(self.factor - 1, -1, -1)], dim=-1)
-              for t in range(xs.size(0)) if (t + 1) % self.factor == 0]
+        xs = [torch.cat([xs[t - r:t - r + 1] for r in range(self.subsampling_factor - 1, -1, -1)], dim=-1)
+              for t in range(xs.size(0)) if (t + 1) % self.subsampling_factor == 0]
         xs = torch.cat(xs, dim=0).transpose(1, 0)
         # NOTE: Exclude the last frames if the length is not divisible
         xs = torch.relu(self.proj(xs))
 
-        xlens = [max(1, math.floor(i // self.factor)) for i in xlens]
+        xlens = [max(1, math.floor(i // self.subsampling_factor)) for i in xlens]
         xlens = torch.IntTensor(xlens)
         return xs, xlens
 
@@ -52,19 +52,19 @@ class ConcatSubsampler(nn.Module):
 class Conv1dSubsampler(nn.Module):
     """Subsample by 1d convolution and max-pooling."""
 
-    def __init__(self, factor, n_units, conv_kernel_size=5):
+    def __init__(self, subsampling_factor, n_units, conv_kernel_size=5):
         super(Conv1dSubsampler, self).__init__()
 
         assert conv_kernel_size % 2 == 1, "Kernel size should be odd for 'same' conv."
-        self.factor = factor
-        if factor > 1:
+        self.subsampling_factor = subsampling_factor
+        if subsampling_factor > 1:
             self.conv1d = nn.Conv1d(in_channels=n_units,
                                     out_channels=n_units,
                                     kernel_size=conv_kernel_size,
                                     stride=1,
                                     padding=(conv_kernel_size - 1) // 2)
-            self.pool = nn.MaxPool1d(kernel_size=factor,
-                                     stride=factor,
+            self.pool = nn.MaxPool1d(kernel_size=subsampling_factor,
+                                     stride=subsampling_factor,
                                      padding=0,
                                      ceil_mode=True)
 
@@ -79,7 +79,7 @@ class Conv1dSubsampler(nn.Module):
             xlens (IntTensor): `[B]` (on CPU)
 
         """
-        if self.factor == 1:
+        if self.subsampling_factor == 1:
             return xs, xlens
 
         xs = torch.relu(self.conv1d(xs.transpose(2, 1)))
@@ -92,10 +92,10 @@ class Conv1dSubsampler(nn.Module):
 class DropSubsampler(nn.Module):
     """Subsample by droping input frames."""
 
-    def __init__(self, factor):
+    def __init__(self, subsampling_factor):
         super(DropSubsampler, self).__init__()
 
-        self.factor = factor
+        self.subsampling_factor = subsampling_factor
 
     def forward(self, xs, xlens):
         """Forward pass.
@@ -108,12 +108,12 @@ class DropSubsampler(nn.Module):
             xlens (IntTensor): `[B]` (on CPU)
 
         """
-        if self.factor == 1:
+        if self.subsampling_factor == 1:
             return xs, xlens
 
-        xs = xs[:, ::self.factor, :]
+        xs = xs[:, ::self.subsampling_factor, :]
 
-        xlens = [max(1, math.floor(i // self.factor)) for i in xlens]
+        xlens = [max(1, math.floor(i // self.subsampling_factor)) for i in xlens]
         xlens = torch.IntTensor(xlens)
         return xs, xlens
 
@@ -121,13 +121,13 @@ class DropSubsampler(nn.Module):
 class MaxpoolSubsampler(nn.Module):
     """Subsample by max-pooling input frames."""
 
-    def __init__(self, factor):
+    def __init__(self, subsampling_factor):
         super(MaxpoolSubsampler, self).__init__()
 
-        self.factor = factor
-        if factor > 1:
-            self.pool = nn.MaxPool1d(kernel_size=factor,
-                                     stride=factor,
+        self.subsampling_factor = subsampling_factor
+        if subsampling_factor > 1:
+            self.pool = nn.MaxPool1d(kernel_size=subsampling_factor,
+                                     stride=subsampling_factor,
                                      padding=0,
                                      ceil_mode=True)
 
@@ -142,7 +142,7 @@ class MaxpoolSubsampler(nn.Module):
             xlens (IntTensor): `[B]` (on CPU)
 
         """
-        if self.factor == 1:
+        if self.subsampling_factor == 1:
             return xs, xlens
 
         xs = self.pool(xs.transpose(2, 1)).transpose(2, 1).contiguous()
