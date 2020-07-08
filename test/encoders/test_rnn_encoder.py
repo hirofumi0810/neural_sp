541b49e4c79919925e8cd2023e084f9e120477a1
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2020-07-08 18:58:26 +0900

    Update test

--- test/encoders/test_rnn_encoder.py
+++ test/encoders/test_rnn_encoder.py
@@ -17,7 +17,7 @@ def make_args(**kwargs):
     args = dict(
         input_dim=80,
         rnn_type='blstm',
-        n_units=64,
+        n_units=16,
         n_projs=0,
         last_proj_dim=0,
         n_layers=4,
@@ -74,15 +74,15 @@ def make_args(**kwargs):
         ({'rnn_type': 'conv_blstm', 'conv_batch_norm': True}),
         ({'rnn_type': 'conv_blstm', 'conv_layer_norm': True}),
         # projection
-        ({'rnn_type': 'blstm', 'n_projs': 32}),
-        ({'rnn_type': 'lstm', 'n_projs': 32}),
+        ({'rnn_type': 'blstm', 'n_projs': 8}),
+        ({'rnn_type': 'lstm', 'n_projs': 8}),
         ({'rnn_type': 'blstm', 'bidir_sum_fwd_bwd': True}),
-        ({'rnn_type': 'blstm', 'bidir_sum_fwd_bwd': True, 'n_projs': 32}),
-        ({'rnn_type': 'blstm', 'last_proj_dim': 10}),
-        ({'rnn_type': 'blstm', 'last_proj_dim': 10, 'n_projs': 32}),
-        ({'rnn_type': 'lstm', 'last_proj_dim': 10, 'n_projs': 32}),
-        ({'rnn_type': 'blstm', 'bidir_sum_fwd_bwd': True, 'last_proj_dim': 10}),
-        ({'rnn_type': 'blstm', 'bidir_sum_fwd_bwd': True, 'last_proj_dim': 10, 'n_projs': 32}),
+        ({'rnn_type': 'blstm', 'bidir_sum_fwd_bwd': True, 'n_projs': 8}),
+        ({'rnn_type': 'blstm', 'last_proj_dim': 5}),
+        ({'rnn_type': 'blstm', 'last_proj_dim': 5, 'n_projs': 8}),
+        ({'rnn_type': 'lstm', 'last_proj_dim': 5, 'n_projs': 8}),
+        ({'rnn_type': 'blstm', 'bidir_sum_fwd_bwd': True, 'last_proj_dim': 5}),
+        ({'rnn_type': 'blstm', 'bidir_sum_fwd_bwd': True, 'last_proj_dim': 5, 'n_projs': 8}),
         # subsampling
         ({'rnn_type': 'blstm', 'subsample': "1_2_2_1_1", 'subsample_type': 'drop'}),
         ({'rnn_type': 'blstm', 'subsample': "1_2_2_1_1", 'subsample_type': 'concat'}),
@@ -97,13 +97,13 @@ def make_args(**kwargs):
         ({'rnn_type': 'blstm', 'subsample': "1_2_2_1_1", 'subsample_type': '1dconv',
           'bidir_sum_fwd_bwd': True}),
         ({'rnn_type': 'blstm', 'subsample': "1_2_2_1_1", 'subsample_type': 'drop',
-          'n_projs': 32}),
+          'n_projs': 8}),
         ({'rnn_type': 'blstm', 'subsample': "1_2_2_1_1", 'subsample_type': 'concat',
-          'n_projs': 32}),
+          'n_projs': 8}),
         ({'rnn_type': 'blstm', 'subsample': "1_2_2_1_1", 'subsample_type': 'max_pool',
-          'n_projs': 32}),
+          'n_projs': 8}),
         ({'rnn_type': 'blstm', 'subsample': "1_2_2_1_1", 'subsample_type': '1dconv',
-          'n_projs': 32}),
+          'n_projs': 8}),
         # LC-BLSTM
         ({'rnn_type': 'blstm', 'chunk_size_right': 40}),  # for PT
         ({'rnn_type': 'blstm', 'chunk_size_left': 40, 'chunk_size_right': 40}),
@@ -111,15 +111,14 @@ def make_args(**kwargs):
           'chunk_size_right': 40}),  # for PT
         ({'rnn_type': 'blstm', 'bidir_sum_fwd_bwd': True,
           'chunk_size_left': 40, 'chunk_size_right': 40}),
-        ({'rnn_type': 'blstm', 'bidir_sum_fwd_bwd': True,
-          'chunk_size_left': 40, 'chunk_size_right': 40,
-          'conv_poolings': "(2,1)_(2,1)"}),
-        ({'rnn_type': 'blstm', 'bidir_sum_fwd_bwd': True,
-          'chunk_size_left': 40, 'chunk_size_right': 40,
-          'conv_poolings': "(1,2)_(1,2)"}),
-        ({'rnn_type': 'blstm', 'bidir_sum_fwd_bwd': True,
-          'chunk_size_left': 40, 'chunk_size_right': 40,
-          'conv_poolings': "(1,1)_(1,1)"}),
+        ({'rnn_type': 'conv_blstm', 'bidir_sum_fwd_bwd': True, 'chunk_size_left': 40, 'chunk_size_right': 40}),
+        # LC-BLSTM + subsampling
+        ({'rnn_type': 'blstm', 'subsample': "1_2_1_1_1", 'chunk_size_right': 40}),  # for PT
+        ({'rnn_type': 'blstm', 'subsample': "1_2_1_1_1", 'chunk_size_left': 40, 'chunk_size_right': 40}),
+        ({'rnn_type': 'blstm', 'subsample': "1_2_1_1_1", 'bidir_sum_fwd_bwd': True,
+          'chunk_size_right': 40}),  # for PT
+        ({'rnn_type': 'blstm', 'subsample': "1_2_1_1_1", 'bidir_sum_fwd_bwd': True,
+          'chunk_size_left': 40, 'chunk_size_right': 40}),
         # Multi-task
         ({'rnn_type': 'blstm', 'n_layers_sub1': 3}),
         ({'rnn_type': 'blstm', 'n_layers_sub1': 3, 'n_layers_sub2': 2}),
@@ -146,21 +145,21 @@ def test_forward(args):
         assert enc_out_dict['ys']['xs'].size(0) == batch_size
         assert enc_out_dict['ys']['xs'].size(1) == enc_out_dict['ys']['xlens'].max()
         for b in range(batch_size):
-            if 'conv' in args['rnn_type'] or args['subsample_type'] in ['max_pool', '1dconv']:
+            if 'conv' in args['rnn_type'] or args['subsample_type'] in ['max_pool', '1dconv', 'drop']:
                 assert enc_out_dict['ys']['xlens'][b].item() == math.ceil(xlens[b].item() / enc.subsampling_factor)
             else:
-                assert enc_out_dict['ys']['xlens'][b].item() == math.floor(xlens[b].item() / enc.subsampling_factor)
+                assert enc_out_dict['ys']['xlens'][b].item() == xlens[b].item() // enc.subsampling_factor
+
         if args['n_layers_sub1'] > 0:
             # all outputs
             assert enc_out_dict['ys_sub1']['xs'].size(0) == batch_size
             assert enc_out_dict['ys_sub1']['xs'].size(1) == enc_out_dict['ys_sub1']['xlens'].max()
             for b in range(batch_size):
-                if 'conv' in args['rnn_type'] or args['subsample_type'] in ['max_pool', '1dconv']:
+                if 'conv' in args['rnn_type'] or args['subsample_type'] in ['max_pool', '1dconv', 'drop']:
                     assert enc_out_dict['ys_sub1']['xlens'][b].item() == math.ceil(
                         xlens[b].item() / enc.subsampling_factor)
                 else:
-                    assert enc_out_dict['ys_sub1']['xlens'][b].item() == math.floor(
-                        xlens[b].item() / enc.subsampling_factor)
+                    assert enc_out_dict['ys_sub1']['xlens'][b].item() == xlens[b].item() // enc.subsampling_factor
             # single output
             enc_out_dict_sub1 = enc(xs, xlens, task='ys_sub1')
             assert enc_out_dict_sub1['ys_sub1']['xs'].size(0) == batch_size
@@ -171,12 +170,11 @@ def test_forward(args):
             assert enc_out_dict['ys_sub2']['xs'].size(0) == batch_size
             assert enc_out_dict['ys_sub2']['xs'].size(1) == enc_out_dict['ys_sub2']['xlens'].max()
             for b in range(batch_size):
-                if 'conv' in args['rnn_type'] or args['subsample_type'] in ['max_pool', '1dconv']:
+                if 'conv' in args['rnn_type'] or args['subsample_type'] in ['max_pool', '1dconv', 'drop']:
                     assert enc_out_dict['ys_sub2']['xlens'][b].item() == math.ceil(
                         xlens[b].item() / enc.subsampling_factor)
                 else:
-                    assert enc_out_dict['ys_sub2']['xlens'][b].item() == math.floor(
-                        xlens[b].item() / enc.subsampling_factor)
+                    assert enc_out_dict['ys_sub2']['xlens'][b].item() == xlens[b].item() // enc.subsampling_factor
             # single output
             enc_out_dict_sub12 = enc(xs, xlens, task='ys_sub2')
             assert enc_out_dict_sub12['ys_sub2']['xs'].size(0) == batch_size
