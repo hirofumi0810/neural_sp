1dbac615f78e3a65d6551bd045db69085f711248
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2020-12-06 20:24:13 +0900

    Update test

--- test/encoders/test_rnn_encoder.py
+++ test/encoders/test_rnn_encoder.py
@@ -55,7 +55,9 @@ def make_args(**kwargs):
         ({'enc_type': 'blstm'}),
         ({'enc_type': 'bgru'}),
         ({'enc_type': 'lstm'}),
+        ({'enc_type': 'lstm', 'rsp_prob': 0.5}),
         ({'enc_type': 'gru'}),
+        ({'enc_type': 'gru', 'rsp_prob': 0.5}),
         # 2dCNN-RNN
         ({'enc_type': 'conv_blstm'}),
         ({'enc_type': 'conv_blstm', 'input_dim': 240, 'conv_in_channel': 3}),
@@ -119,6 +121,8 @@ def make_args(**kwargs):
           'chunk_size_left': "40", 'chunk_size_right': "40"}),
         ({'enc_type': 'conv_blstm', 'bidir_sum_fwd_bwd': True,
           'chunk_size_left': "40", 'chunk_size_right': "40"}),
+        ({'enc_type': 'conv_blstm', 'bidir_sum_fwd_bwd': True,
+          'chunk_size_left': "40", 'chunk_size_right': "40", 'rsp_prob': 0.5}),
         # LC-BLSTM + subsampling
         ({'enc_type': 'blstm', 'subsample': "1_2_1_1_1",
           'chunk_size_right': "40"}),  # for PT
@@ -128,13 +132,19 @@ def make_args(**kwargs):
           'chunk_size_right': "40"}),  # for PT
         ({'enc_type': 'blstm', 'subsample': "1_2_1_1_1", 'bidir_sum_fwd_bwd': True,
           'chunk_size_left': "40", 'chunk_size_right': "40"}),
+        ({'enc_type': 'blstm', 'subsample': "1_2_1_1_1", 'bidir_sum_fwd_bwd': True,
+          'chunk_size_left': "40", 'chunk_size_right': "40", 'rsp_prob': 0.5}),
         # Multi-task
         ({'enc_type': 'blstm', 'n_layers_sub1': 2}),
         ({'enc_type': 'blstm', 'n_layers_sub1': 2, 'task_specific_layer': True}),
         ({'enc_type': 'blstm', 'n_layers_sub1': 2, 'chunk_size_right': "40"}),
-        ({'enc_type': 'blstm', 'n_layers_sub1': 2, 'chunk_size_right': "40", 'task_specific_layer': True}),
+        ({'enc_type': 'blstm', 'n_layers_sub1': 2, 'chunk_size_right': "40",
+          'task_specific_layer': True}),
+        ({'enc_type': 'blstm', 'n_layers_sub1': 2, 'chunk_size_right': "40",
+          'task_specific_layer': True, 'rsp_prob': 0.5}),
         ({'enc_type': 'blstm', 'n_layers_sub1': 2, 'n_layers_sub2': 1}),
-        ({'enc_type': 'blstm', 'n_layers_sub1': 2, 'n_layers_sub2': 1, 'task_specific_layer': True}),
+        ({'enc_type': 'blstm', 'n_layers_sub1': 2, 'n_layers_sub2': 1,
+          'task_specific_layer': True}),
     ]
 )
 def test_forward(args):
