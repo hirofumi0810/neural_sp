ff4f05976c51ff2090b42e8bcc2e03aabcf031c6
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2020-12-18 16:07:02 +0900

    Add local/score.sh

---
+++ examples/ci_test/local/score.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Copyright 2020 Kyoto University (Hirofumi Inaguma)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+model=
+gpu=
+stdout=false
+
+### path to save preproecssed data
+data=./data
+
+batch_size=1
+beam_width=5
+min_len_ratio=0.0
+max_len_ratio=1.0
+length_penalty=0.1
+coverage_penalty=0.0
+coverage_threshold=0.0
+gnmt_decoding=false
+ctc_weight=0.0  # 1.0 for joint CTC-attention means decoding with CTC
+n_average=2  # for Transformer
+
+. ./cmd.sh
+. ./path.sh
+. utils/parse_options.sh
+
+set -e
+set -u
+set -o pipefail
+
+if [ -z ${gpu} ]; then
+    n_gpus=0
+else
+    n_gpus=$(echo ${gpu} | tr "," "\n" | wc -l)
+fi
+
+for set in train; do
+    recog_dir=$(dirname ${model})/decode_${set}_beam${beam_width}_lp${length_penalty}_cp${coverage_penalty}_${min_len_ratio}_${max_len_ratio}
+    if [ ${ctc_weight} != 0.0 ]; then
+        recog_dir=${recog_dir}_ctc${ctc_weight}
+    fi
+    if [ ${gnmt_decoding} = true ]; then
+        recog_dir=${recog_dir}_gnmt
+    fi
+    mkdir -p ${recog_dir}
+
+    CUDA_VISIBLE_DEVICES=${gpu} coverage run --append ${NEURALSP_ROOT}/neural_sp/bin/asr/eval.py \
+        --recog_n_gpus ${n_gpus} \
+        --recog_sets ${data}/dataset/${set}_char.tsv \
+        --recog_dir ${recog_dir} \
+        --recog_model ${model} \
+        --recog_batch_size ${batch_size} \
+        --recog_beam_width ${beam_width} \
+        --recog_max_len_ratio ${max_len_ratio} \
+        --recog_min_len_ratio ${min_len_ratio} \
+        --recog_length_penalty ${length_penalty} \
+        --recog_coverage_penalty ${coverage_penalty} \
+        --recog_coverage_threshold ${coverage_threshold} \
+        --recog_gnmt_decoding ${gnmt_decoding} \
+        --recog_ctc_weight ${ctc_weight} \
+        --recog_n_average ${n_average} \
+        --recog_stdout ${stdout} || exit 1;
+done
