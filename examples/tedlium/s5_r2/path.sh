2b154a788164d74a1f85953c38634fcc89df6382
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2020-12-30 03:37:18 +0900

    Minor fix

--- examples/tedlium/s5_r2/path.sh
+++ examples/tedlium/s5_r2/path.sh
@@ -14,8 +14,7 @@ export LC_ALL=C
 ### Python
 source $CONDA/etc/profile.d/conda.sh && conda deactivate && conda activate
 export PYTHONDONTWRITEBYTECODE=1
-# export OMP_NUM_THREADS=1
-export OMP_NUM_THREADS=2
+export OMP_NUM_THREADS=1
 
 ### CUDA
 CUDAROOT=/usr/local/cuda
