Fix oracle and streamability calculation for longform evaluation

neural_sp/evaluators/character.py
@@ -157,7 +157,7 @@ def eval_char(models, dataloader, recog_params, epoch,
                                             for hyp_n in nbest_hyps[1:]]
                         oracle_idx = np.argmin(np.array(cers_b))
                         if oracle_idx == 0:
-                            n_oracle_hit += 1
+                            n_oracle_hit += len(batch['utt_ids'])
                         cer_oracle += cers_b[oracle_idx]
 
                     if fine_grained:
@@ -168,7 +168,7 @@ def eval_char(models, dataloader, recog_params, epoch,
                             cer_dist[xlen_bin] = [err_b / 100]
 
                     if models[0].streamable():
-                        n_streamable += 1
+                        n_streamable += len(batch['utt_ids'])
                     else:
                         last_success_frame_ratio += models[0].last_success_frame_ratio()
                     quantity_rate += models[0].quantity_rate()

neural_sp/evaluators/phone.py
@@ -23,8 +23,8 @@ def eval_phone(models, dataloader, recog_params, epoch,
         models (List): models to evaluate
         dataloader (torch.utils.data.DataLoader): evaluation dataloader
         recog_params (omegaconf.dictconfig.DictConfig): decoding hyperparameters
-        epoch (int):
-        recog_dir (str):
+        epoch (int): current epoch
+        recog_dir (str): directory path to save hypotheses
         streaming (bool): streaming decoding for the session-level evaluation
         progressbar (bool): visualize the progressbar
         fine_grained (bool): calculate fine-grained PER distributions based on input lengths
@@ -115,7 +115,7 @@ def eval_phone(models, dataloader, recog_params, epoch,
                                             for hyp_n in nbest_hyps[1:]]
                         oracle_idx = np.argmin(np.array(pers_b))
                         if oracle_idx == 0:
-                            n_oracle_hit += 1
+                            n_oracle_hit += len(batch['utt_ids'])
                         per_oracle += pers_b[oracle_idx]
 
                 n_utt += len(batch['utt_ids'])

neural_sp/evaluators/word.py
@@ -25,8 +25,8 @@ def eval_word(models, dataloader, recog_params, epoch,
         models (List): models to evaluate
         dataloader (torch.utils.data.DataLoader): evaluation dataloader
         recog_params (omegaconf.dictconfig.DictConfig): decoding hyperparameters
-        epoch (int):
-        recog_dir (str):
+        epoch (int): current epoch
+        recog_dir (str): directory path to save hypotheses
         streaming (bool): streaming decoding for session-level evaluation
         progressbar (bool): visualize progressbar
         fine_grained (bool): calculate fine-grained WER distributions based on input lengths
@@ -161,7 +161,7 @@ def eval_word(models, dataloader, recog_params, epoch,
                                             for hyp_n in nbest_hyps[1:]]
                         oracle_idx = np.argmin(np.array(wers_b))
                         if oracle_idx == 0:
-                            n_oracle_hit += 1
+                            n_oracle_hit += len(batch['utt_ids'])
                         wer_oracle += wers_b[oracle_idx]
                         # NOTE: OOV resolution is not considered
 