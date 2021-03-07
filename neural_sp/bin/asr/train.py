e48e3855ddfe123c6694485f8c1285a21177d35b
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2021-03-07 19:57:14 +0900

    Refactor training ASR script

--- neural_sp/bin/asr/train.py
+++ neural_sp/bin/asr/train.py
@@ -48,8 +48,6 @@ from neural_sp.trainers.optimizer import set_optimizer
 from neural_sp.trainers.reporter import Reporter
 from neural_sp.utils import mkdir_join
 
-torch.manual_seed(1)
-torch.cuda.manual_seed_all(1)
 
 logger = logging.getLogger(__name__)
 
@@ -57,6 +55,10 @@ logger = logging.getLogger(__name__)
 def main():
 
     args = parse_args_train(sys.argv[1:])
+
+    torch.manual_seed(1)
+    torch.cuda.manual_seed_all(1)
+
     args_init = copy.deepcopy(args)
     args_teacher = copy.deepcopy(args)
 
@@ -112,20 +114,20 @@ def main():
 
     # Set save path
     if args.resume:
-        save_path = os.path.dirname(args.resume)
-        dir_name = os.path.basename(save_path)
+        args.save_path = os.path.dirname(args.resume)
+        dir_name = os.path.basename(args.save_path)
     else:
         dir_name = set_asr_model_name(args)
         if args.mbr_training:
             assert args.asr_init
-            save_path = mkdir_join(os.path.dirname(args.asr_init), dir_name)
+            args.save_path = mkdir_join(os.path.dirname(args.asr_init), dir_name)
         else:
-            save_path = mkdir_join(args.model_save_dir, '_'.join(
+            args.save_path = mkdir_join(args.model_save_dir, '_'.join(
                 os.path.basename(args.train_set).split('.')[:-1]), dir_name)
-        save_path = set_save_path(save_path)  # avoid overwriting
+        args.save_path = set_save_path(args.save_path)  # avoid overwriting
 
     # Set logger
-    set_logger(os.path.join(save_path, 'train.log'), stdout=args.stdout)
+    set_logger(os.path.join(args.save_path, 'train.log'), stdout=args.stdout)
 
     # Load a LM conf file for LM fusion & LM initialization
     if not args.resume and args.external_lm:
@@ -137,22 +139,22 @@ def main():
         assert args.vocab == args.lm_conf.vocab
 
     # Model setting
-    model = Speech2Text(args, save_path, train_set.idx2token[0])
+    model = Speech2Text(args, args.save_path, train_set.idx2token[0])
 
     if not args.resume:
         # Save conf file as a yaml file
-        save_config(args, os.path.join(save_path, 'conf.yml'))
+        save_config(args, os.path.join(args.save_path, 'conf.yml'))
         if args.external_lm:
-            save_config(args.lm_conf, os.path.join(save_path, 'conf_lm.yml'))
+            save_config(args.lm_conf, os.path.join(args.save_path, 'conf_lm.yml'))
 
         # Save nlsyms, dictionary, and wp_model
         if args.nlsyms:
-            shutil.copy(args.nlsyms, os.path.join(save_path, 'nlsyms.txt'))
+            shutil.copy(args.nlsyms, os.path.join(args.save_path, 'nlsyms.txt'))
         for sub in ['', '_sub1', '_sub2']:
             if args.get('dict' + sub):
-                shutil.copy(args.get('dict' + sub), os.path.join(save_path, 'dict' + sub + '.txt'))
+                shutil.copy(args.get('dict' + sub), os.path.join(args.save_path, 'dict' + sub + '.txt'))
             if args.get('unit' + sub) == 'wp':
-                shutil.copy(args.get('wp_model' + sub), os.path.join(save_path, 'wp' + sub + '.model'))
+                shutil.copy(args.get('wp_model' + sub), os.path.join(args.save_path, 'wp' + sub + '.model'))
 
         for k, v in sorted(args.items(), key=lambda x: x[0]):
             logger.info('%s: %s' % (k, str(v)))
@@ -244,19 +246,17 @@ def main():
         load_checkpoint(args.teacher_lm, teacher_lm)
 
     # GPU setting
-    use_apex = args.train_dtype in ["O0", "O1", "O2", "O3"]
-    amp = None
+    args.use_apex = args.train_dtype in ["O0", "O1", "O2", "O3"]
+    amp, scaler = None, None
     if args.n_gpus >= 1:
         model.cudnn_setting(deterministic=not (is_transformer or args.cudnn_benchmark),
                             benchmark=not is_transformer and args.cudnn_benchmark)
-        model.cuda()
 
         # Mixed precision training setting
-        if use_apex:
+        if args.use_apex:
             if LooseVersion(torch.__version__) >= LooseVersion("1.6.0"):
                 scaler = torch.cuda.amp.GradScaler()
             else:
-                scaler = None
                 from apex import amp
                 model, scheduler.optimizer = amp.initialize(model, scheduler.optimizer,
                                                             opt_level=args.train_dtype)
@@ -266,6 +266,8 @@ def main():
                 amp.init()
                 if args.resume:
                     load_checkpoint(args.resume, amp=amp)
+
+        model.cuda()
         model = CustomDataParallel(model, device_ids=list(range(0, args.n_gpus)))
 
         if teacher is not None:
@@ -282,12 +284,13 @@ def main():
     setproctitle(args.job_name if args.job_name else dir_name)
 
     # Set reporter
-    reporter = Reporter(save_path)
+    reporter = Reporter(args.save_path)
 
+    # Define tasks
     if args.mtl_per_batch:
         # NOTE: from easier to harder tasks
         tasks = []
-        if 1 - args.bwd_weight - args.ctc_weight - args.sub1_weight - args.sub2_weight > 0:
+        if args.total_weight - args.bwd_weight - args.ctc_weight - args.sub1_weight - args.sub2_weight > 0:
             tasks += ['ys']
         if args.bwd_weight > 0:
             tasks = ['ys.bwd'] + tasks
@@ -310,148 +313,39 @@ def main():
         model.module.trigger_quantity_loss()
 
     start_time_train = time.time()
-    start_time_epoch = time.time()
-    start_time_step = time.time()
-    accum_n_steps = 0
     n_steps = scheduler.n_steps * accum_grad_n_steps
-    epoch_detail_prev = 0
     for ep in range(resume_epoch, args.n_epochs):
-        pbar_epoch = tqdm(total=len(train_set))
-        session_prev = None
-        for batch_train, is_new_epoch in train_set:
-            # Compute loss in the training set
-            if args.discourse_aware and batch_train['sessions'][0] != session_prev:
-                model.module.reset_session()
-            session_prev = batch_train['sessions'][0]
-            accum_n_steps += 1
+        n_steps = train(model, train_set, dev_set, eval_sets,
+                        scheduler, reporter, logger, args,
+                        tasks, n_steps, accum_grad_n_steps, amp, scaler,
+                        teacher, teacher_lm)
 
-            # Change mini-batch depending on task
-            if accum_n_steps == 1:
-                loss_train = 0  # moving average over gradient accumulation
-            for task in tasks:
-                if use_apex and scaler is not None:
-                    with torch.cuda.amp.autocast():
-                        loss, observation = model(batch_train, task=task,
-                                                  teacher=teacher, teacher_lm=teacher_lm)
-                else:
-                    loss, observation = model(batch_train, task=task,
-                                              teacher=teacher, teacher_lm=teacher_lm)
-                loss = loss / accum_grad_n_steps
-                reporter.add(observation)
-                if use_apex:
-                    if scaler is not None:
-                        scaler.scale(loss).backward()
-                    else:
-                        with amp.scale_loss(loss, scheduler.optimizer) as scaled_loss:
-                            scaled_loss.backward()
-                else:
-                    loss.backward()
-                loss.detach()  # Truncate the graph
-                if accum_n_steps >= accum_grad_n_steps or is_new_epoch:
-                    if args.clip_grad_norm > 0:
-                        total_norm = torch.nn.utils.clip_grad_norm_(
-                            model.module.parameters(), args.clip_grad_norm)
-                        reporter.add_tensorboard_scalar('total_norm', total_norm)
-                    if use_apex and scaler is not None:
-                        scaler.step(scheduler.optimizer)
-                        scaler.update()
-                        scheduler.step(skip_optimizer=True)  # update lr only
-                    else:
-                        scheduler.step()
-                    scheduler.zero_grad()
-                    accum_n_steps = 0
-                    # NOTE: parameters are forcibly updated at the end of every epoch
-                loss_train += loss.item()
-                del loss
-
-            pbar_epoch.update(len(batch_train['utt_ids']))
-            reporter.add_tensorboard_scalar('learning_rate', scheduler.lr)
-            # NOTE: loss/acc/ppl are already added in the model
-            reporter.step()
-            n_steps += 1
-            # NOTE: n_steps is different from the step counter in Noam Optimizer
-
-            if n_steps % args.print_step == 0:
-                # Compute loss in the dev set
-                batch_dev = iter(dev_set).next(batch_size=1 if 'transducer' in args.dec_type else None)[0]
-                # Change mini-batch depending on task
-                for task in tasks:
-                    loss, observation = model(batch_dev, task=task, is_eval=True)
-                    reporter.add(observation, is_eval=True)
-                    loss_dev = loss.item()
-                    del loss
-                reporter.step(is_eval=True)
-
-                duration_step = time.time() - start_time_step
-                if args.input_type == 'speech':
-                    xlen = max(len(x) for x in batch_train['xs'])
-                    ylen = max(len(y) for y in batch_train['ys'])
-                elif args.input_type == 'text':
-                    xlen = max(len(x) for x in batch_train['ys'])
-                    ylen = max(len(y) for y in batch_train['ys_sub1'])
-                logger.info("step:%d(ep:%.2f) loss:%.3f(%.3f)/lr:%.7f/bs:%d/xlen:%d/ylen:%d (%.2f min)" %
-                            (n_steps, scheduler.n_epochs + train_set.epoch_detail,
-                             loss_train, loss_dev,
-                             scheduler.lr, len(batch_train['utt_ids']),
-                             xlen, ylen, duration_step / 60))
-                start_time_step = time.time()
-
-            # Save figures of loss and accuracy
-            if n_steps % (args.print_step * 10) == 0:
-                reporter.snapshot()
-                model.module.plot_attention()
-                model.module.plot_ctc()
-
-            # Ealuate model every 0.1 epoch during MBR training
-            if args.mbr_training:
-                if int(train_set.epoch_detail * 10) != int(epoch_detail_prev * 10):
-                    sub_epoch = int(train_set.epoch_detail * 10) / 10
-                    # dev
-                    metric_dev = evaluate([model.module], dev_set, args, sub_epoch, logger)
-                    reporter.epoch(metric_dev, name=args.metric)  # plot
-                    # Save model
-                    scheduler.save_checkpoint(
-                        model, save_path, remove_old=False, amp=amp,
-                        epoch_detail=sub_epoch)
-                    # test
-                    for eval_set in eval_sets:
-                        evaluate([model.module], eval_set, args, sub_epoch, logger)
-                epoch_detail_prev = train_set.epoch_detail
-
-            if is_new_epoch:
-                break
-
-        # Save checkpoint and evaluate model per epoch
-        duration_epoch = time.time() - start_time_epoch
-        logger.info('========== EPOCH:%d (%.2f min) ==========' %
-                    (scheduler.n_epochs + 1, duration_epoch / 60))
-
+        # Save checkpoint and validate model per epoch
         if scheduler.n_epochs + 1 < args.eval_start_epoch:
             scheduler.epoch()  # lr decay
             reporter.epoch()  # plot
 
             # Save model
             scheduler.save_checkpoint(
-                model, save_path, remove_old=not is_transformer and args.remove_old_checkpoints, amp=amp)
+                model, args.save_path, remove_old=not is_transformer and args.remove_old_checkpoints, amp=amp)
         else:
             start_time_eval = time.time()
             # dev
-            metric_dev = evaluate([model.module], dev_set, args, scheduler.n_epochs + 1, logger)
+            metric_dev = validate([model.module], dev_set, args, scheduler.n_epochs + 1, logger)
             scheduler.epoch(metric_dev)  # lr decay
             reporter.epoch(metric_dev, name=args.metric)  # plot
 
             if scheduler.is_topk or is_transformer:
                 # Save model
                 scheduler.save_checkpoint(
-                    model, save_path, remove_old=not is_transformer and args.remove_old_checkpoints, amp=amp)
+                    model, args.save_path, remove_old=not is_transformer and args.remove_old_checkpoints, amp=amp)
 
                 # test
                 if scheduler.is_topk:
                     for eval_set in eval_sets:
-                        evaluate([model.module], eval_set, args, scheduler.n_epochs, logger)
+                        validate([model.module], eval_set, args, scheduler.n_epochs, logger)
 
-            duration_eval = time.time() - start_time_eval
-            logger.info('Evaluation time: %.2f min' % (duration_eval / 60))
+            logger.info('Evaluation time: %.2f min' % ((time.time() - start_time_eval) / 60))
 
             # Early stopping
             if scheduler.is_early_stop:
@@ -469,20 +363,138 @@ def main():
         if args.get('mocha_quantity_loss_start_epoch', 0) == (ep + 1):
             model.module.trigger_quantity_loss()
 
-        start_time_step = time.time()
-        start_time_epoch = time.time()
-
-    duration_train = time.time() - start_time_train
-    logger.info('Total time: %.2f hour' % (duration_train / 3600))
-
+    logger.info('Total time: %.2f hour' % ((time.time() - start_time_train) / 3600))
     reporter.tf_writer.close()
+
+    return args.save_path
+
+
+def train(model, train_set, dev_set, eval_sets,
+          scheduler, reporter, logger, args,
+          tasks, n_steps, accum_grad_n_steps, amp, scaler,
+          teacher, teacher_lm):
+    """Train model for one epoch."""
+    pbar_epoch = tqdm(total=len(train_set))
+    session_prev = None
+    accum_n_steps = 0  # reset at every epoch
+    epoch_detail_prev = train_set.epoch_detail
+    start_time_step = time.time()
+    start_time_epoch = time.time()
+    for batch_train, is_new_epoch in train_set:
+        # Compute loss in the training set
+        if args.discourse_aware and batch_train['sessions'][0] != session_prev:
+            model.module.reset_session()
+        session_prev = batch_train['sessions'][0]
+        accum_n_steps += 1
+
+        # Change mini-batch depending on task
+        if accum_n_steps == 1:
+            loss_train = 0  # moving average over gradient accumulation
+        for task in tasks:
+            if args.use_apex and scaler is not None:
+                with torch.cuda.amp.autocast():
+                    loss, observation = model(batch_train, task=task,
+                                              teacher=teacher, teacher_lm=teacher_lm)
+            else:
+                loss, observation = model(batch_train, task=task,
+                                          teacher=teacher, teacher_lm=teacher_lm)
+            loss /= accum_grad_n_steps
+
+            # report
+            reporter.add(observation)
+
+            if args.use_apex:
+                if scaler is not None:
+                    scaler.scale(loss).backward()
+                else:
+                    with amp.scale_loss(loss, scheduler.optimizer) as scaled_loss:
+                        scaled_loss.backward()
+            else:
+                loss.backward()
+
+            loss.detach()  # Truncate the graph
+            if accum_n_steps >= accum_grad_n_steps or is_new_epoch:
+                if args.clip_grad_norm > 0:
+                    total_norm = torch.nn.utils.clip_grad_norm_(
+                        model.module.parameters(), args.clip_grad_norm)
+                    reporter.add_tensorboard_scalar('total_norm', total_norm)
+                if args.use_apex and scaler is not None:
+                    scaler.step(scheduler.optimizer)
+                    scaler.update()
+                    scheduler.step(skip_optimizer=True)  # update lr only
+                else:
+                    scheduler.step()
+                scheduler.zero_grad()
+                accum_n_steps = 0
+                # NOTE: parameters are forcibly updated at the end of every epoch
+            loss_train += loss.item()
+            del loss
+
+        pbar_epoch.update(len(batch_train['utt_ids']))
+        reporter.add_tensorboard_scalar('learning_rate', scheduler.lr)
+        # NOTE: loss/acc/ppl are already added in the model
+        reporter.step()
+        n_steps += 1
+        # NOTE: n_steps is different from the step counter in Noam Optimizer
+
+        if n_steps % args.print_step == 0:
+            # Compute loss in the dev set
+            batch_dev = iter(dev_set).next(batch_size=1 if 'transducer' in args.dec_type else None)[0]
+            # Change mini-batch depending on task
+            for task in tasks:
+                loss, observation = model(batch_dev, task=task, is_eval=True)
+                reporter.add(observation, is_eval=True)
+                loss_dev = loss.item()
+                del loss
+            reporter.step(is_eval=True)
+
+            if args.input_type == 'speech':
+                xlen = max(len(x) for x in batch_train['xs'])
+                ylen = max(len(y) for y in batch_train['ys'])
+            elif args.input_type == 'text':
+                xlen = max(len(x) for x in batch_train['ys'])
+                ylen = max(len(y) for y in batch_train['ys_sub1'])
+            logger.info("step:%d(ep:%.2f) loss:%.3f(%.3f)/lr:%.7f/bs:%d/xlen:%d/ylen:%d (%.2f min)" %
+                        (n_steps, scheduler.n_epochs + train_set.epoch_detail,
+                         loss_train, loss_dev,
+                         scheduler.lr, len(batch_train['utt_ids']),
+                         xlen, ylen, (time.time() - start_time_step) / 60))
+            start_time_step = time.time()
+
+        # Save figures of loss and accuracy
+        if n_steps % (args.print_step * 10) == 0:
+            reporter.snapshot()
+            model.module.plot_attention()
+            model.module.plot_ctc()
+
+        # Ealuate model every 0.1 epoch during MBR training
+        if args.mbr_training:
+            if int(train_set.epoch_detail * 10) != int(epoch_detail_prev * 10):
+                sub_epoch = int(train_set.epoch_detail * 10) / 10
+                # dev
+                metric_dev = validate([model.module], dev_set, args, sub_epoch, logger)
+                reporter.epoch(metric_dev, name=args.metric)  # plot
+                # Save model
+                scheduler.save_checkpoint(
+                    model, args.save_path, remove_old=False, amp=amp,
+                    epoch_detail=sub_epoch)
+                # test
+                for eval_set in eval_sets:
+                    validate([model.module], eval_set, args, sub_epoch, logger)
+            epoch_detail_prev = train_set.epoch_detail
+
+        if is_new_epoch:
+            break
+
+    logger.info('========== EPOCH:%d (%.2f min) ==========' %
+                (scheduler.n_epochs + 1, (time.time() - start_time_epoch) / 60))
     pbar_epoch.close()
 
-    return save_path
+    return n_steps
 
 
-def evaluate(models, dataloader, args, epoch, logger):
-
+def validate(models, dataloader, args, epoch, logger):
+    """Validate performance per epoch."""
     if args.metric == 'edit_distance':
         if args.unit in ['word', 'word_char']:
             metric = eval_word(models, dataloader, args, epoch=epoch)[0]
